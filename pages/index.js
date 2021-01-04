0eea78b212480b40a96d923775dccc5d2b8bbaf3
Tushar Debnath <41472705+tushar7d@users.noreply.github.com>
2021-01-04 18:04:50 +0530

    changes

--- pages/index.js
+++ pages/index.js
@@ -4,18 +4,20 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Create Tushar App</title>
+        <title>Tushar Debbnath</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="container p-3 pt-10">
-        <div
-          class=" text-3xl font-bold text-left  "
-          children={`Hello`}
-        />
-        <div
-          class=" text-l font-regular text-left "
-          children={`I am Tushar Debnath a product designer from New Delhi`}
-        />
+      <div className="container p-1 max-w-full pt-16  h-screen bg-red-100">
+        <div className="text-center text-l font-normal mb-1 text-gray-700">
+          Hello👋 , I am
+        </div>
+        <div className="text-center text-4xl  font-semibold">তুষার দেবনাথ</div>
+        <div className="container max-w-full p-12">
+          <div className="rounded bg-white shadow-sm w-full p-4 mb-2">
+            About
+          </div>
+          <div className="rounded bg-white shadow-sm w-full p-4 mb-2">Work</div>
+        </div>
       </div>
     </div>
   );
