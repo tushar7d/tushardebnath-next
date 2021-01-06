0f4a0680617cf4d53848c860e04f5bb839f9813d
Tushar Debnath <41472705+tushar7d@users.noreply.github.com>
2021-01-06 11:22:41 +0530

    changes

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <div className=" max-w-screen-xl w-screen m-auto h-screen ">
       <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-6 p-8">
-        <Card size="2" color="bg-muted-blue">hello</Card>
+        <Card size="2" color="bg-muted-blue"><Intro /></Card>
         <Card size="1" color="bg-muted-green">hello</Card>
         <Card size="1" color="bg-muted-red">red</Card>
         <Card size="2" color="bg-muted-purple">purple</Card>
@@ -11,4 +11,13 @@ export default function Home() {
       </div>
     </div>
   );
+}
+let Intro = () =>{
+  return(
+    <div className="h-ful  flex">
+
+      <div className="w-full">hello</div>
+      <div className="w-full">hello</div>
+    </div>
+  )
 }
