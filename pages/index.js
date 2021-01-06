699c02db5532900fd975f87d5342a7a4aaa7e192
Tushar Debnath <41472705+tushar7d@users.noreply.github.com>
2021-01-06 22:18:14 +0530

    changes

--- pages/index.js
+++ pages/index.js
@@ -8,16 +8,16 @@ export default function Home() {
           <Intro />
         </Card>
         <Card size="xl:col-span-1" color="bg-muted-green">
-          hello
+          <Heading />
         </Card>
         <Card size="xl:col-span-1" color="bg-muted-red">
-          red
+        <Heading />
         </Card>
         <Card size="xl:col-span-2" color="bg-muted-purple">
-          purple
+        <Heading />
         </Card>
         <Card size="xl:col-span-3" color="bg-muted-yellow">
-          hello
+        <Heading />
         </Card>
       </div>
     </div>
@@ -26,18 +26,33 @@ export default function Home() {
 let Intro = () => {
   return (
     <div className="h-full grid grid-cols-1 md:grid-cols-3 gap-2 ">
-      <div className="h-full md:col-span-2 flex flex-col justify-center items-center  md:items-start p-3 ">
-        <div className="text-3xl text-center md:text-4xl xl:text-5xl  lg:text-left font-serif ">
-          Tushar Debnath
+      <div className="h-full md:col-span-2 flex flex-col   md:items-start p-3  pt-24">
+        <div className="text-2xl text-center md:text-4xl xl:text-4xl  lg:text-left font-bold ">
+        Hello, I am Tushar Debnath a Product Designer & Design Technologist 
         </div>
-        <div className="text-xl  xl:text-2xl text-gray-500 text-center lg:text-left">
+        <div className="text-xl  xl:text-xl text-gray-500 text-center lg:text-left mt-1">
           Product Designer, & Design Technologist
         </div>
+        <button className="bg-white rounded-full px-6 py-2 mt-6 text-lg ">
+          Know more
+        </button>
       </div>
 
       <div className="h-full flex justify-center  items-center ">
-        <div className=" bg-white  w-36 h-36 rounded-full" />
+        <div className=" bg-white  w-full h-full opacity-20 " />
       </div>
     </div>
   );
+}; 
+
+let Heading = () => {
+  return <div className="h-full p-3">
+    <h1 className=" text-center sm:text-left text-2xl  font-semibold">
+      Figma Plugin
+    </h1>
+    <div className="text-gray-500 text-center sm:text-left">
+      A few figma plugin I have been working on.
+    </div>
+
+  </div>;
 };
