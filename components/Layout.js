96c3800ba93b50b53cad62a4ca17a1f66d9688d6
Tushar Debnath <41472705+tushar7d@users.noreply.github.com>
2021-01-07 00:20:00 +0530

    added typography

--- components/Layout.js
+++ components/Layout.js
@@ -1,16 +1,14 @@
-import Head from 'next/head'
-import Container from './Container'
+import Head from "next/head";
+import Container from "./Container";
 
-export default function 
-Layout(props) {
-    return (
-      <Container>
-          <Head>
+export default function Layout(props) {
+  return (
+    <Container>
+      <Head>
         <title>{props.meta.title}</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-        {props.children}
-      </Container>
-    );
-  }
-  
+      <div className="prose pt-12 w-3/4 m-auto">{props.children}</div>
+    </Container>
+  );
+}
