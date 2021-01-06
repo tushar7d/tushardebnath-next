233bd541e4f71a3879500cd19819a484aacb8b05
Tushar Debnath <41472705+tushar7d@users.noreply.github.com>
2021-01-07 00:12:21 +0530

    changes

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Card from "../components/Card";
-import Container from '../components/Container';
+import Container from "../components/Container";
 
 export default function Home() {
   return (
@@ -9,31 +9,58 @@ export default function Home() {
           <Intro />
         </Card>
         <Card size="xl:col-span-1" color="bg-muted-green">
-          <Heading head="Figma Plugin" sub="A few cool plugins I built from scratch" />
+          <Heading
+            head="Figma Plugin"
+            sub="A few cool plugins I built from scratch"
+          />
         </Card>
         <Card size="xl:col-span-1" color="bg-muted-red">
-        <Heading head="Figma Plugin" sub="A few cool plugins I built from scratch" />
+          <Heading
+            head="Figma Plugin"
+            sub="A few cool plugins I built from scratch"
+          />
         </Card>
         <Card size="xl:col-span-2" color="bg-muted-purple">
-        <Heading head="Figma Plugin" sub="A few cool plugins I built from scratch" />
+          <Heading
+            head="Figma Plugin"
+            sub="A few cool plugins I built from scratch"
+          />
         </Card>
         <Card size="xl:col-span-1" color="bg-muted-yellow">
-        <Heading  head="Figma Plugin" sub="A few cool plugins I built from scratch" />
+          <Heading
+            head="Figma Plugin"
+            sub="A few cool plugins I built from scratch"
+          />
         </Card>
         <Card size="xl:col-span-1" color="bg-muted-green">
-        <Heading  head="Figma Plugin" sub="A few cool plugins I built from scratch" />
+          <Heading
+            head="Figma Plugin"
+            sub="A few cool plugins I built from scratch"
+          />
         </Card>
         <Card size="xl:col-span-1" color="bg-muted-red">
-        <Heading  head="Figma Plugin" sub="A few cool plugins I built from scratch" />
+          <Heading
+            head="Figma Plugin"
+            sub="A few cool plugins I built from scratch"
+          />
         </Card>
         <Card size="xl:col-span-1" color="bg-muted-purple">
-        <Heading  head="Figma Plugin" sub="A few cool plugins I built from scratch" />
+          <Heading
+            head="Figma Plugin"
+            sub="A few cool plugins I built from scratch"
+          />
         </Card>
         <Card size="xl:col-span-1" color="bg-muted-yellow">
-        <Heading  head="Figma Plugin" sub="A few cool plugins I built from scratch" />
+          <Heading
+            head="Figma Plugin"
+            sub="A few cool plugins I built from scratch"
+          />
         </Card>
         <Card size="xl:col-span-1" color="bg-muted-blue">
-        <Heading  head="Figma Plugin" sub="A few cool plugins I built from scratch" />
+          <Heading
+            head="Figma Plugin"
+            sub="A few cool plugins I built from scratch"
+          />
         </Card>
       </div>
     </Container>
@@ -44,12 +71,12 @@ let Intro = () => {
     <div className="h-full grid grid-cols-1 gap-1 sm:grid-cols-3">
       <div className="h-full flex flex-col p-3 sm:col-span-2 justify-center sm:items-start  ">
         <div className="text-2xl text-center font-bold sm:text-2xl  md:text-4xl xl:text-4xl   sm:text-left  ">
-        Hello, I am Tushar Debnath a Product Designer & Design Technologist 
+          Hello, I am Tushar Debnath a Product Designer & Design Technologist
         </div>
         <div className="text-xl  xl:text-xl text-gray-500 text-center   sm:text-left mt-1">
           Product Designer, & Design Technologist
         </div>
-        <button className="bg-white rounded-full px-6 py-2 mt-6 text-lg ">
+        <button className="bg-white rounded-full px-6 py-2 mt-6 text-lg hover:shadow sm:shadow-md ">
           Know more
         </button>
       </div>
@@ -59,16 +86,13 @@ let Intro = () => {
       </div>
     </div>
   );
-}; 
+};
 
 let Heading = (props) => {
-  return <div className="h-full p-3">
-    <h1 className="  text-2xl  font-semibold">
-      {props.head}
-    </h1>
-    <div className="text-gray-500 ">
-      {props.sub}
+  return (
+    <div className="h-full p-3">
+      <h1 className="  text-2xl  font-semibold">{props.head}</h1>
+      <div className="text-gray-500 ">{props.sub}</div>
     </div>
-
-  </div>;
+  );
 };
