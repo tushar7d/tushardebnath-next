73b919a2110d3a0087298aec0de8bdf20870d6ee
Tushar Debnath <41472705+tushar7d@users.noreply.github.com>
2021-01-06 23:40:13 +0530

    changes

--- pages/index.js
+++ pages/index.js
@@ -25,9 +25,9 @@ export default function Home() {
 }
 let Intro = () => {
   return (
-    <div className="h-full grid grid-cols-1   sm:grid-cols-3 gap-1 ">
-      <div className="h-full sm:col-span-2 flex flex-col   md:items-start p-3 sm:pt-16  md:pt-24">
-        <div className="text-2xl  sm:text-2xl  md:text-4xl xl:text-4xl text-center  sm:text-left font-bold ">
+    <div className="h-full grid grid-cols-1 gap-1 sm:grid-cols-3">
+      <div className="h-full flex flex-col p-3 sm:col-span-2 justify-center sm:items-start  ">
+        <div className="text-2xl text-center font-bold sm:text-2xl  md:text-4xl xl:text-4xl   sm:text-left  ">
         Hello, I am Tushar Debnath a Product Designer & Design Technologist 
         </div>
         <div className="text-xl  xl:text-xl text-gray-500 text-center   sm:text-left mt-1">
