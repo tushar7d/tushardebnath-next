fce7bd0f2e86d89f1fbcad16c8064dd7ab59e4f4
Tushar Debnath <41472705+tushar7d@users.noreply.github.com>
2021-01-08 20:38:41 +0530

    changes

--- components/Cards/PluginList.js
+++ components/Cards/PluginList.js
@@ -2,9 +2,9 @@ import ListContainer from "./ListContainer";
 
 let PluginList = () => {
   return (
-    <ListContainer heading="Recent Figma plugins">
-      <Entry heading="blahhhh" />
-      <Entry heading="blahhhh" />
+    <ListContainer heading="Design Tools">
+      <Entry heading="Project Scaffold" />
+      <Entry heading="ColorKit" />
     </ListContainer>
   );
 };
@@ -12,7 +12,7 @@ export default PluginList;
 
 let Entry = (props) => {
   return (
-    <div className="flex items-center  py-4">
+    <div className="flex items-center py-3">
       <div className="w-12 h-12 bg-gray-200 rounded-full" />
 
       <div className="ml-2">
