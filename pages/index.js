ec18713be792532dd3aaf7a0650149c389c38914
Tushar Debnath <41472705+tushar7d@users.noreply.github.com>
2021-01-08 16:07:39 +0530

    changes

--- pages/index.js
+++ pages/index.js
@@ -14,15 +14,84 @@ export default function Home() {
         <Card size="sm:col-span-2 " color="bg-gray-100">
           <Intro />
         </Card>
-        <Card size="sm:col-span-2 md:col-span-1" color=" bg-gray-100" >Blog List</Card>
-        <Card size="sm:col-span-2 md:col-span-1" color=" bg-gray-100" >Plugin List</Card>
-        <Card size="sm:col-span-2 md:col-span-1" color=" bg-gray-100" to="/work/crystal">Crystal</Card>
-        <Card size="sm:col-span-2 md:col-span-1" color=" bg-gray-100" to="/work/variants">Variants</Card>
-        <Card size="sm:col-span-2 md:col-span-1" color=" bg-gray-100" to="/work/automation">Design Automation</Card>
-        <Card size="sm:col-span-2 md:col-span-1" color=" bg-gray-100" to="/work/bluepackets">Blue Packets</Card>
-        <Card size="sm:col-span-2 md:col-span-1" color=" bg-gray-100" to="/work/searchexperience">Search Experience</Card>
-
+        <Card size="sm:col-span-2 md:col-span-1" color=" bg-gray-100">
+          <BlogList />{" "}
+        </Card>
+        <Card size="sm:col-span-2 md:col-span-1" color=" bg-gray-100">
+          Plugin List
+        </Card>
+        <Card
+          size="sm:col-span-2 md:col-span-1"
+          color=" bg-gray-100"
+          to="/work/crystal"
+        >
+          Crystal
+        </Card>
+        <Card
+          size="sm:col-span-2 md:col-span-1"
+          color=" bg-gray-100"
+          to="/work/variants"
+        >
+          Variants
+        </Card>
+        <Card
+          size="sm:col-span-2 md:col-span-1"
+          color=" bg-gray-100"
+          to="/work/automation"
+        >
+          Design Automation
+        </Card>
+        <Card
+          size="sm:col-span-2 md:col-span-1"
+          color=" bg-gray-100"
+          to="/work/bluepackets"
+        >
+          Blue Packets
+        </Card>
+        <Card
+          size="sm:col-span-2 md:col-span-1"
+          color=" bg-gray-100"
+          to="/work/searchexperience"
+        >
+          Search Experience
+        </Card>
       </Grid>
     </Container>
   );
 }
+
+let BlogList = () => {
+  return (
+    <div className="h-full p-2">
+      <div className=" text-2xl font-semibold mb-4">Writing</div>
+      <div className="divide-y-2">
+        <div className="my-4">
+          <div className="  text-base font-medium mt-4 ">
+            How to run Figma shortcuts using Midi.
+          </div>
+          <div className="  text-base font-normal text-gray-400 ">
+            Medium Article
+          </div>
+        </div>
+        <div className="my-4">
+          <div className=" text-base  font-medium  mt-4">
+            How I created a Figma plugin to automate our design system
+            migration.
+          </div>
+          <div className="  text-base font-normal text-gray-400">
+            Medium Article
+          </div>
+        </div>
+        <div className="my-4">
+          <div className=" text-base  font-medium mt-4">
+            How structuring Figma files in a consistent way has improved
+            collaboration and efficiency.
+          </div>
+          <div className="  text-base font-normal text-gray-400">
+            Medium Article
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
