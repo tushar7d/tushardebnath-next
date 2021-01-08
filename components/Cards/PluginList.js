28c463fdb8b419cb0021938ba5fb10b2afc9b630
Tushar Debnath <41472705+tushar7d@users.noreply.github.com>
2021-01-08 20:50:15 +0530

    changes

--- components/Cards/PluginList.js
+++ components/Cards/PluginList.js
@@ -3,8 +3,9 @@ import ListContainer from "./ListContainer";
 let PluginList = () => {
   return (
     <ListContainer heading="Design Tools">
-      <Entry heading="Project Scaffold" />
-      <Entry heading="ColorKit" />
+      <Entry heading="Project Scaffold" sub="A opensource plugin to scaffold out a common file structure for your figma files" />
+      <Entry heading="ColorKit" sub="A opensource plugin to scaffold out a common file structure for your figma files" />
+      <Entry heading="ColorKit" sub="A opensource plugin to scaffold out a common file structure for your figma files" />
     </ListContainer>
   );
 };
@@ -13,10 +14,11 @@ export default PluginList;
 let Entry = (props) => {
   return (
     <div className="flex items-center py-3">
-      <div className="w-12 h-12 bg-gray-200 rounded-full" />
+      <div className=" w-12 h-12 bg-gray-200  rounded-sm" />
 
       <div className="ml-2">
         <div className=" font-medium">{props.heading}</div>
+        <div className=" text-gray-500 text-sm">{props.sub}</div>
       </div>
     </div>
   );
