a993be66a8794c14f1efe4d4db76944be52f9117
Tushar Debnath <41472705+tushar7d@users.noreply.github.com>
2021-01-13 12:00:54 +0530

    changes

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,10 @@
 
 import Container from "./Container";
-import NavBar from '../components/NavBar'
+
 
 export default function Layout(props) {
   return (
     <Container title={props.meta.title}>
-      <NavBar />
       <div className="prose pt-12  w-5/6 m-auto">{props.children}</div>
     </Container>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,69 +1,18 @@
-
 import Card from "../components/Card";
 import Container from "../components/Container";
 import Intro from "../components/Cards/Intro";
 import Grid from "../components/Grid";
-import NavBar from '../components/NavBar'
-
 
 export default function Home() {
   return (
     <Container title="tushar debnath">
-      
       <Grid>
-        <Card size="sm:col-span-2 " color="bg-gray-100">
+        <Card size="col-span-1 sm:col-span-2 " color="bg-gray-100">
           <Intro />
         </Card>
+        <Card size="col-span-1 "  color="bg-gray-100"/>
 
-        <Card
-          size="sm:col-span-2 md:col-span-1"
-          color=" bg-gray-100"
-          to="/work/crystal"
-        >
-          <div className="h-full p-2 overflow-y-hidden">
-            <div className=" text-2xl font-semibold mb-4">Crystal</div>
-          </div>
-        </Card>
-        <Card
-          size="sm:col-span-2 md:col-span-1"
-          color=" bg-gray-100"
-          to="/work/variants"
-        >
-          <div className="h-full p-2 overflow-y-hidden">
-            <div className=" text-2xl font-semibold mb-4">Variants</div>
-          </div>
-        </Card>
-        <Card
-          size="sm:col-span-2 md:col-span-1"
-          color=" bg-gray-100"
-          to="/work/automation"
-        >
-          <div className="h-full p-2 overflow-y-hidden">
-            <div className=" text-2xl font-semibold mb-4">
-              Design Automation
-            </div>
-          </div>
-        </Card>
-        <Card
-          size="sm:col-span-2 md:col-span-1"
-          color=" bg-gray-100"
-          to="/work/bluepackets"
-        >
-          <div className="h-full p-2 overflow-y-hidden">
-            <div className=" text-2xl font-semibold mb-4">Blue Packets</div>
-          </div>
-        </Card>
-        <Card
-          size="sm:col-span-2 md:col-span-1"
-          color=" bg-gray-100"
-          to="/work/searchexperience"
-        >
-          <div className="h-full p-2 overflow-y-hidden">
-            <div className=" text-2xl font-semibold mb-4">
-              Search Experience
-            </div>
-          </div>
-        </Card>
+        
       </Grid>
     </Container>
   );
