a1ec05169aafd2c1dceb93a1d044b6ccd51f4c41
Tushar Debnath <41472705+tushar7d@users.noreply.github.com>
2021-04-19 15:16:27 +0530

    Created branch and changed copy

--- components/Cards/Intro.js
+++ components/Cards/Intro.js
@@ -2,12 +2,12 @@ import Image from "next/image";
 
 let Intro = () => {
   let Container = (props) => (
-    <div className=" h-full flex flex-col-reverse sm:flex-row ">
+    <div className=" h-full flex flex-col-reverse sm:flex-row  ">
       {props.children}
     </div>
   );
   let Left = (props) => (
-    <div className="flex flex-col sm:items-start justify-center w-full sm:w-2/3 ">
+    <div className="flex flex-col sm:items-start justify-center w-full sm:w-2/3 sm:ml-4 ">
       {props.children}
     </div>
   );
@@ -24,15 +24,15 @@ let Intro = () => {
           Hey I'm Tushar 👋
         </div>
         <div className="text-l   text-gray-500 text-center   sm:text-left mt-1 leading-6">
-          I'm a Product designer and a design technologist. Currently helping millions order food online
+          I'm a product designer and a design technologist. Currently helping millions order food online
           at Zomato. 
           <br />
           <br />
           In the past I have worked with Expedia, Hike Messenger and MakeMyTrip building products at 
-          a global scale and also worked on developing design systems and design tools internally.
+          a global scale and helping to build design systems and developing internal design tools, process and.
           <br />
           <br />
-          I am also a big beliver of opensource so I have a few opensource figma plugins I am super proud of. </div>
+          I am a big beliver in community learning and opensource and I actively work on opensource projects and do my part in giving back to this awesome community. </div>
         <div className="text-blue-800 my-3 font-semibold">Know More</div>
       </Left>
 
