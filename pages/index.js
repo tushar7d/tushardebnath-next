5903cbd31374ce0ed9c8eb9546367296b1a5a89b
Tushar Debnath <txdebnath@gmail.com>
2021-05-02 21:20:45 +0530

    changes

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,22 @@
 
 export default function Home() {
   return (
-    <div className="prose mt-12 mx-auto">
+    <div className="prose mt-12 mx-auto px-4">
       <h1>
-        Hello
+        Hello, I'm Tushar Debnath.
       </h1>
-      
-      <p>
-        I'm Tushar Debnath, a Product designer and a design technologist from New Delhi. Currently helping millions order food online
-        at Zomato. In the past I have worked with Expedia, Hike Messenger and MakeMyTrip building products at
-        a global scale and also worked on developing design systems and design tools internally.I am also a big
-        beliver of opensource so I have a few opensource figma plugins I am super proud of.
-      </p>
 
-      
+      <p>
+        A Product designer and a design technologist from New Delhi. Currently helping millions order food online
+        at Zomato. In the past I have worked with Expedia, Hike Messenger and MakeMyTrip building products at
+        a global scale, developing design systems and develop internal tools.
+      </p>
+      <div className="flex">
+        <a className="mr-5" href="https://www.linkedin.com/in/tushardebnath/">Linkedin</a>
+        <a className="mr-5" href="https://dribbble.com/tushardebnath">Dribbble</a>
+      </div>
+
+
     </div>
   );
 }
