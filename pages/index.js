f95184788c0bf22d57ce96a7c638e80127c66594
Tushar Debnath <txdebnath@gmail.com>
2021-05-02 21:09:00 +0530

    changes

--- pages/index.js
+++ pages/index.js
@@ -1,136 +1,19 @@
 
 export default function Home() {
   return (
-    <div className=" mt-4">
+    <div className="prose mt-12 mx-auto">
       <h1>
-        Hello, I’m Tushar Debnath.
+        Hello
       </h1>
+      
       <p>
-        I'm a Product designer and a design technologist. Currently helping millions order food online
+        I'm Tushar Debnath, a Product designer and a design technologist from New Delhi. Currently helping millions order food online
         at Zomato. In the past I have worked with Expedia, Hike Messenger and MakeMyTrip building products at
         a global scale and also worked on developing design systems and design tools internally.I am also a big
         beliver of opensource so I have a few opensource figma plugins I am super proud of.
       </p>
 
-      <h3 className="mt-8">
-        Select Work
-      </h3>
-
-      <ul className="mt-2">
-
-        <li className="mt-4">
-            <div>
-              <p>Tag</p>
-              <h3>Title</h3>
-              <p>DEscription in long form text</p>
-            </div>
-
-        </li>
-        <li className="mt-4">
-            <div>
-              <p>Tag</p>
-              <h3>Title</h3>
-              <p>DEscription in long form text</p>
-            </div>
-
-        </li>
-        <li className="mt-4">
-            <div>
-              <p>Tag</p>
-              <h3>Title</h3>
-              <p>DEscription in long form text</p>
-            </div>
-
-        </li>
-        <li className="mt-4">
-            <div>
-              <p>Tag</p>
-              <h3>Title</h3>
-              <p>DEscription in long form text</p>
-            </div>
-
-        </li>
-      </ul>
-
-
-      <h3 className="mt-8">
-        Writing
-      </h3>
-
-      <ul className="mt-2">
-
-        <li className="mt-4">
-            <div>
-              <p>Tag</p>
-              <h3>Title</h3>
-              <p>DEscription in long form text</p>
-            </div>
-
-        </li>
-        <li className="mt-4">
-            <div>
-              <p>Tag</p>
-              <h3>Title</h3>
-              <p>DEscription in long form text</p>
-            </div>
-
-        </li>
-        <li className="mt-4">
-            <div>
-              <p>Tag</p>
-              <h3>Title</h3>
-              <p>DEscription in long form text</p>
-            </div>
-
-        </li>
-        <li className="mt-4">
-            <div>
-              <p>Tag</p>
-              <h3>Title</h3>
-              <p>DEscription in long form text</p>
-            </div>
-
-        </li>
-      </ul>
-      <h3 className="mt-8">
-        Figma plugin
-      </h3>
-
-      <ul className="mt-2">
-
-        <li className="mt-4">
-            <div>
-              <p>Tag</p>
-              <h3>Title</h3>
-              <p>DEscription in long form text</p>
-            </div>
-
-        </li>
-        <li className="mt-4">
-            <div>
-              <p>Tag</p>
-              <h3>Title</h3>
-              <p>DEscription in long form text</p>
-            </div>
-
-        </li>
-        <li className="mt-4">
-            <div>
-              <p>Tag</p>
-              <h3>Title</h3>
-              <p>DEscription in long form text</p>
-            </div>
-
-        </li>
-        <li className="mt-4">
-            <div>
-              <p>Tag</p>
-              <h3>Title</h3>
-              <p>DEscription in long form text</p>
-            </div>
-
-        </li>
-      </ul>
+      
     </div>
   );
 }
