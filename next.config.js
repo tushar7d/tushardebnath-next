00aa41daa91cb60e1ec15d22b957aabf900050fd
Tushar Debnath <txdebnath@gmail.com>
2021-05-03 08:53:20 +0530

    move to webpack 5

--- next.config.js
+++ next.config.js
@@ -2,5 +2,7 @@ const withMDX = require('@next/mdx')({
     extension: /\.mdx?$/,
   })
   module.exports = withMDX({
-    pageExtensions: ['js', 'jsx', 'mdx'],
+    pageExtensions: ['js', 'jsx', 'mdx'],future: {
+      webpack5: true,
+    },
   })
