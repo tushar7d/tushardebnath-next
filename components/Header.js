a61fd1909006229130763e7e4fc2adc2dd0c1cb5
Tushar Debnath <41472705+tushar7d@users.noreply.github.com>
2021-05-14 22:54:39 +0530

    changes

--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,8 @@ import { useTheme } from 'next-themes'
 export default function Header(props) {
     const { theme, setTheme } = useTheme()
     return (
-        <div className="flex justify-between items-center sticky top-0 bg-white max-w-prose mx-auto ">
+        <div className=" sticky top-0 bg-white dark:bg-black  border-b border-solid dark:border-transparent">
+          <div className=" flex justify-between items-center max-w-prose mx-auto">
         <h3 className="text-xl font-medium  ml-4 dark:text-white">Home</h3>
         <button
           aria-label="Toggle Dark Mode"
@@ -11,7 +12,10 @@ export default function Header(props) {
           onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
         ><svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
             <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
-          </svg></button></div>
+          </svg></button>
+          
+          </div>
+          </div>
     );
   }
   
