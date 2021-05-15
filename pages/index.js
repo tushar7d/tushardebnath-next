8328131a5092e07d38d0962a077a7593ecc3b800
Tushar Debnath <txdebnath@gmail.com>
2021-05-15 19:49:19 +0530

    changes

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,8 @@
-import Header from "../components/Header";
 import Head from "next/head";
 
 import ReactRotatingText from "react-rotating-text";
 import { faLinkedin, faDribbbleSquare, faGithubSquare } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
 
 export default function Home() {
   return (
@@ -14,89 +12,87 @@ export default function Home() {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       
-      <div className="container prose mx-auto max-w-prose  ">
-      
-        <div className="  mt-12 mx-auto px-8 md:px-4">
+      <div className=" prose max-w-prose mx-auto  mt-12 px-8 md:px-4  ">
+      <img
+          src="/td.png"
+          alt="Picture of the Me"
+          style={{ maxWidth: "150px" }}
+        />
+         <h1 className=" dark:text-white  ">Hello, I'm Tushar Debnath.</h1>
+        
+       
+        <div className="flex">
+        <div className="text-2xl mr-1 dark:text-white">I design </div>
+        <div className=" text-2xl text-yellow-500  font-medium ">
+
           
-           <img
-              src="/td.png"
-              alt="Picture of the Me"
-              style={{maxWidth:"150px"}}
-            />
-          
+            <ReactRotatingText 
+            items={[" products.", " custom tools.", " workflows.", " systems."]}
+          />
+        </div>
+        </div>
 
-          <h1 className=" dark:text-white  ">Hello,I'm Tushar Debnath.</h1>
-          <div className="dark:text-white text-2xl text-black font-medium ">
-
-            I design
-            <ReactRotatingText
-              items={[" products.", " custom tools.", " workflows.", " systems."]}
-            />
-          </div>
-
-          <p className=" text-gray-500 dark:text-white">
-            I am currently helping millions order food online at Zomato. In the
-            past I have worked with Expedia, Hike Messenger and MakeMyTrip
-            building products at a global scale, developing design systems and
-            internal tools. 
+        <p className=" text-gray-500 dark:text-white">
+          I am currently helping millions order food online at Zomato. In the
+          past I have worked with Expedia, Hike Messenger and MakeMyTrip
+          building products at a global scale, developing design systems and
+          internal tools.
           </p>
-          
-          <div className="flex text-3xl ">
-           
-            <a
-              className="mr-5 dark:text-white no-underline "
-              target="_blank"
-              href="https://www.linkedin.com/in/tushardebnath/"
-            >
-              <FontAwesomeIcon icon={faLinkedin} className="text-gray-700 dark:text-white" /> 
-            </a>
-            <a
-              className="mr-5 dark:text-white"
-              target="_blank"
-              href="https://dribbble.com/tushardebnath"
-            >
-               <FontAwesomeIcon icon={faDribbbleSquare} className="text-gray-700 dark:text-white" /> 
-            </a>
-            <a
-              className="mr-5 dark:text-white"
-              target="_blank"
-              href="https://github.com/tushar7d"
-            >
-              <FontAwesomeIcon icon={faGithubSquare} className="text-gray-700 dark:text-white" /> 
-            </a>
-          </div>
 
-          <h2 className="dark:text-white">Writing</h2>
-          <div className="mb-3">
-            <a
-              className="text-l no-underline text-gray-500 dark:text-white"
-              target="_blank"
-              href="https://blog.prototypr.io/how-to-run-figma-shortcuts-using-midi-e0ab0997eafa"
-            >
-              How to run Figma shortcuts using Midi.
-            </a>
-          </div>
-          <div className="mb-3">
-            <a
-              className="text-l no-underline text-gray-500 dark:text-white"
-              target="_blank"
-              href="https://blog.prototypr.io/how-i-created-a-figma-plugin-to-automate-our-design-system-migration-600d1c07518e"
-            >
-              Automating our design system migration @ Expedia.
-            </a>
-          </div>
-          <div>
-            <a
-              className="text-l no-underline text-gray-500 dark:text-white"
-              target="_blank"
-              href="https://blog.prototypr.io/how-structuring-figma-files-in-a-consistent-way-has-improved-collaboration-and-efficiency-f3c904791b85"
-            >
-              Better way to structure design files.
-            </a>
-          </div>
+        <div className="flex text-3xl ">
 
-          <h2 className="dark:text-white">Figma Plugins</h2>
-          <div className="mb-3">
+          <a
+            className="mr-5 dark:text-white no-underline "
+            target="_blank"
+            href="https://www.linkedin.com/in/tushardebnath/"
+          >
+            <FontAwesomeIcon icon={faLinkedin} className="text-gray-700 dark:text-white  hover:text-yellow-500 dark:hover:text-yellow-500" />
+          </a>
+          <a
+            className="mr-5 dark:text-white"
+            target="_blank"
+            href="https://dribbble.com/tushardebnath"
+          >
+            <FontAwesomeIcon icon={faDribbbleSquare} className="text-gray-700 dark:text-white hover:text-yellow-500 dark:hover:text-yellow-500" />
+          </a>
+          <a
+            className="mr-5 dark:text-white"
+            target="_blank"
+            href="https://github.com/tushar7d"
+          >
+            <FontAwesomeIcon icon={faGithubSquare} className="text-gray-700 dark:text-white hover:text-yellow-500 dark:hover:text-yellow-500" />
+          </a>
+        </div>
+        <h3 className="dark:text-white text-yellow-400">Writing</h3>
+        <div className="mb-3">
+          <a
+            className="text-l no-underline text-gray-500 dark:text-white"
+            target="_blank"
+            href="https://blog.prototypr.io/how-to-run-figma-shortcuts-using-midi-e0ab0997eafa"
+          >
+            How to run Figma shortcuts using Midi.
+            </a>
+        </div>
+        <div className="mb-3">
+          <a
+            className="text-l no-underline text-gray-500 dark:text-white"
+            target="_blank"
+            href="https://blog.prototypr.io/how-i-created-a-figma-plugin-to-automate-our-design-system-migration-600d1c07518e"
+          >
+            Automating our design system migration @ Expedia.
+            </a>
+        </div>
+        <div>
+          <a
+            className="text-l no-underline text-gray-500 dark:text-white"
+            target="_blank"
+            href="https://blog.prototypr.io/how-structuring-figma-files-in-a-consistent-way-has-improved-collaboration-and-efficiency-f3c904791b85"
+          >
+            Better way to structure design files.
+            </a>
+        </div>
+        <h3 className="dark:text-white">Figma Plugins</h3>
+        <div className="mb-3">
             <a
               className="text-l no-underline text-gray-500 dark:text-white"
               target="_blank"
@@ -107,6 +103,7 @@ export default function Home() {
               
             </a>
           </div>
+
           <div className="mb-3">
             <a
               className="text-l no-underline text-gray-500 dark:text-white"
@@ -126,8 +123,13 @@ export default function Home() {
               <div className=" text-gray-800 dark:text-white hover:text-blue-800">Cover Generator</div>
             </a>
           </div>
-        </div>
+          
+          
+          
       </div>
+
+
+
     </>
   );
 }
