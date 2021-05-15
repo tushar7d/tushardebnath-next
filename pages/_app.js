a93ed00ff4750321cf0bbae4374abe5d21329dad
Tushar Debnath <41472705+tushar7d@users.noreply.github.com>
2021-05-16 01:00:53 +0530

    changes

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class" defaultTheme="system">
       <Header />
+      
       <Component {...pageProps} />
     </ThemeProvider>
   );
