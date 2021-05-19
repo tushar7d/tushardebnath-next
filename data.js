d860d218aaa31e79ab91a6443942d3d71be78338
Tushar Debnath <txdebnath@gmail.com>
2021-05-19 18:02:06 +0530

    changes

---
+++ data.js
@@ -0,0 +1,8 @@
+let projects =[
+    {
+        type="PRODUCT DESIGN",
+        title="Launching grocery at Zomato",
+        subtitle="A case study detailing how we launched the grocery purchase experience in record time"
+
+        },
+]
