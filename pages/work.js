fde2d6ee7d963de91f06684a3c050a3dfcc1ba43
Tushar Debnath <41472705+tushar7d@users.noreply.github.com>
2021-05-19 10:39:18 +0530

    chages

--- pages/work.js
+++ pages/work.js
@@ -32,9 +32,9 @@ export default function Work() {
         <div className="my-12 mx-auto px-8 md:px-4">
         <Card
           type="PRODUCT DESIGN"
-            img="/crystal.png"
-            color="bg-blue-50 dark:bg-blue-500"
-            title="Grocery at Zomato"
+            img="/grocery.png"
+            color="bg-purple-50 dark:bg-blue-500"
+            title="Launching grocery at Zomato"
             subtitle="A case study detailing how we launched the grocery purchase experience in record time"
           />
           <Card
@@ -48,13 +48,13 @@ export default function Work() {
           type="DESIGN OPS"
             img="/cars.png"
             color="bg-yellow-50 dark:bg-yellow-500"
-            title="Auto correct for design files"
-            subtitle="An internal figma plugin designed to help reduce human error while using the Expedia design system. "
+            title="Design Ops initiatives at Expedia"
+            subtitle="An collection of design ops work done at expedia "
           />
           <Card
           type="PRODUCT DESIGN"
             img="/cars.png"
-            color="bg-red-50 dark:bg-red-500"
+            color="bg-yellow-50 dark:bg-red-500"
             title="Helping users find the right rental car"
             subtitle="A case study detailing how we redesigned the car selection experience for the car rental business at Expedia . "
           />
@@ -71,6 +71,13 @@ export default function Work() {
             color="bg-red-50 dark:bg-red-500"
             title="Better Search for Makemytrip"
             subtitle="Re-Imagining the mobile search user experience"
+          />
+           <Card
+          type="PRODUCT DESIGN"
+            img="/cars.png"
+            color="bg-red-50 dark:bg-red-500"
+            title="Post booking experience"
+            subtitle="Optimized the post booking experiene to minimize support costs"
           />
         </div>
       </div>
