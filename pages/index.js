f8fb002158635b7c7d38bfe064e18016655d39ff
Tushar Debnath <41472705+tushar7d@users.noreply.github.com>
2021-08-28 07:28:29 +0530

    Fixed left bar

--- pages/index.js
+++ pages/index.js
@@ -2,135 +2,40 @@ import Head from "next/head";
 import ReactRotatingText from "react-rotating-text";
 import IconLink from "../components/IconLink";
 
-let ExtLink = () => {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      className="inline-block w-5 h-5 align-text-bottom"
-      viewBox="0 0 20 20"
-      fill="currentColor"
-    >
-      <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z" />
-      <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" />
-    </svg>
-  );
-};
-let Temp = () => {
-  return (
-    <>
-      <h2 className="mt-12 hp-heading">Selected Projects</h2>
-      <div className="mb-3">
-        <a
-          className="link-text"
-          target="_blank"
-          href="https://tushardebnath.notion.site/Helping-users-find-the-right-offer-4140d01adf3343dc98249fdb91d643e2"
-        >
-          Expedia Car Finder <ExtLink />
-        </a>
-      </div>
-      <div className="mb-3">
-        <a
-          className="link-text"
-          target="_blank"
-          href="https://tushardebnath.notion.site/Zomato-Order-Tracker-bb8ca43d32bb4363a1dfe0643c4cb91c"
-        >
-          Zomato Order Tracker <ExtLink />
-        </a>
-      </div>
 
-      <h2 className="mt-12 hp-heading">Recent Writing</h2>
-      <div className="mb-3">
-        <a
-          className="link-text"
-          target="_blank"
-          href="https://blog.prototypr.io/how-to-run-figma-shortcuts-using-midi-e0ab0997eafa"
-        >
-          How to run Figma shortcuts using Midi. <ExtLink />
-        </a>
-      </div>
-      <div className="mb-3">
-        <a
-          className="link-text"
-          target="_blank"
-          href="https://blog.prototypr.io/how-i-created-a-figma-plugin-to-automate-our-design-system-migration-600d1c07518e"
-        >
-          Automating our design system migration @ Expedia. <ExtLink />
-        </a>
-      </div>
-      <div>
-        <a
-          className="link-text"
-          target="_blank"
-          href="https://blog.prototypr.io/how-structuring-figma-files-in-a-consistent-way-has-improved-collaboration-and-efficiency-f3c904791b85"
-        >
-          Better way to structure design files. <ExtLink />
-        </a>
-      </div>
-      <h2 className="hp-heading"> Latest Figma Plugins</h2>
-      <div className="mb-3">
-        <a
-          className="link-text "
-          target="_blank"
-          href="https://www.figma.com/community/plugin/797696673804519719/Color-Kit"
-        >
-          ColorKit <ExtLink />
-        </a>
-      </div>
 
-      <div className="mb-3">
-        <a
-          className="link-text"
-          target="_blank"
-          href="https://www.figma.com/community/plugin/747372158567878238/Project-Scaffold"
-        >
-          Project Scaffold <ExtLink />
-        </a>
-      </div>
-      <div className="mb-3">
-        <a
-          className="link-text"
-          target="_blank"
-          href="https://www.figma.com/community/plugin/745146759001708690/Cover-Generator"
-        >
-          Cover Generator <ExtLink />
-        </a>
-      </div>
-    </>
-  );
-};
 let Intro = () => {
   return (
-    <>
-      <div>
-        <img src="/td.png" alt="Me" style={{ maxWidth: "150px" }} />
-      </div>
+    <div >
+      
+      <img src="/td.png" alt="Me" style={{ maxWidth: "150px" }} />
 
-      <h1 className="title">Hello, I'm Tushar Debnath</h1>
-
-      <div className="flex ">
-        <div className="mr-1 text-xl font-medium dark:text-white">
+      <h1 className="mt-6 mb-2 text-3xl  font-semibold  dark:text-white">Hello, I'm Tushar Debnath</h1>
+     
+      
+      <div className="flex mt-2">
+        <div className="mr-1 text-2xl font-medium dark:text-white">
           I design{" "}
         </div>
-        <div className="text-xl font-medium text-yellow-500 ">
+        <div className="text-2xl font-medium text-yellow-500 ">
           <ReactRotatingText
             items={[" products.", " custom tools.", " workflows.", " systems."]}
           />
         </div>
       </div>
 
-      <p className="hp-p">
-        I am a product designer & design technologist living in New Delhi.
+      <p className="my-4  text-lg text-gray-500">
         Currently, I am working at Zomato. When I am not designing or coding,
         you can find me reading fantasy books, working on my podcast, or
         travelling with my wife.
       </p>
-    </>
+    </div>
   );
 };
 
 let Left = () => {
   return (
-    <div className=" md:w-1/2 flex-shrink-0">
+    <div className=" p-12 md:h-screen md:w-1/2  flex flex-col justify-between ">
       <Intro />
       <div className="flex text-l ">
         <IconLink link="https://www.linkedin.com/in/tushardebnath/">
@@ -181,7 +86,7 @@ let Left = () => {
 };
 let CardList = () => {
   return (
-    <div className="md:w-1/2 mt-4 md:p-4 overflow-y-auto grid grid-cols-1 gap-2">
+    <div className="md:w-1/2 p-6   overflow-y-auto grid grid-cols-1 gap-2">
       <Card
         h="Helping users find the right car"
         dc="Expedia"
@@ -236,7 +141,7 @@ export default function Home() {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
 
-      <div className=" md:flex max-w-7xl m-auto p-12 md:overflow-hidden  max-h-screen">
+      <div className=" md:flex max-w-7xl m-auto  md:overflow-hidden    max-h-screen">
         <Left />
 
         <CardList />
