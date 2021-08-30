7bdf9e4cb674032003e54a0f6f2803978d7a6d1c
Tushar Debnath <txdebnath@gmail.com>
2021-08-30 12:09:11 +0530

    changes

--- components/Content.js
+++ components/Content.js
@@ -1,8 +1,16 @@
+let SectionHeading = (props) => {
+  return (
+    <h1 className="mt-6 mb-2 text-xl font-semibold md:mt-12">
+      {props.children}
+    </h1>
+  );
+};
+
 let Content = () => {
-    return (
-      <div className="overflow-auto no-scrollbar md:w-1/2">
-        <div className="grid grid-cols-1 gap-4 p-4 md:p-8">
-          <h1 className="text-xl font-semibold md:mt-24 ">Work</h1>
+  return (
+    <div className="mb-8 overflow-auto no-scrollbar md:w-1/2">
+      <div className="grid grid-cols-1 gap-4 p-4 md:p-8">
+        <SectionHeading>Selected Works</SectionHeading>
         <Card
           h="Helping users find the right car"
           dc="Expedia"
@@ -13,7 +21,7 @@ let Content = () => {
           dc="Zomato"
           l="https://tushardebnath.notion.site/Zomato-Order-Tracker-bb8ca43d32bb4363a1dfe0643c4cb91c"
         />
-          <h1 className="text-xl font-semibold md:mt-12 ">Writing</h1>
+        <SectionHeading>Recent writing</SectionHeading>
         <Card
           h="Helping users find the right car"
           dc="Expedia"
@@ -24,7 +32,7 @@ let Content = () => {
           dc="Zomato"
           l="https://tushardebnath.notion.site/Zomato-Order-Tracker-bb8ca43d32bb4363a1dfe0643c4cb91c"
         />
-              <h1 className="text-xl font-semibold md:mt-12 ">Opensource </h1>
+        <SectionHeading>Open source</SectionHeading>
         <Card
           h="Helping users find the right car"
           dc="Expedia"
@@ -35,20 +43,19 @@ let Content = () => {
           dc="Zomato"
           l="https://tushardebnath.notion.site/Zomato-Order-Tracker-bb8ca43d32bb4363a1dfe0643c4cb91c"
         />
-        </div>
       </div>
-    );
-  };
-  let Card = (props) => {
-    return (
-      <a href={props.l ? props.l : "/"} className="w-full " target="_blank">
-        <div className="p-4 transition duration-500 ease-in-out bg-white border border-gray-200 rounded-lg md:hover:scale-105 hover:shadow-lg">
-          <h1 className="mb-1 font-semibold ">{props.h ? props.h : "Heading"}</h1>
-          <p>{props.dc ? props.dc : "description"}</p>
-        </div>
-      </a>
-    );
-  };
+    </div>
+  );
+};
+let Card = (props) => {
+  return (
+    <a href={props.l ? props.l : "/"} className="w-full " target="_blank">
+      <div className="p-4 transition duration-500 ease-in-out bg-white border border-gray-200 rounded-lg md:hover:scale-105 hover:shadow-lg">
+        <h1 className="mb-1 font-semibold ">{props.h ? props.h : "Heading"}</h1>
+        <p>{props.dc ? props.dc : "description"}</p>
+      </div>
+    </a>
+  );
+};
 
-  export default Content
-
+export default Content;
