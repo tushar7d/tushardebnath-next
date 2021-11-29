f769ddf95da4195ce16e5076d260e122280bc233
Tushar Debnath <txdebnath@gmail.com>
2021-11-29 21:46:52 +0530

    changes

--- pages/index.js
+++ pages/index.js
@@ -8,10 +8,10 @@ export default function Home() {
         <title>Tushar Debnath</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <div className="">
+      
         <header className="sticky top-0 z-50 w-screen px-8 py-4 bg-white shadow">
           <div className="flex items-center justify-between px-4 mx-auto">
-            <div className="text-3xl font-semibold">T Debnath.</div>
+            <div className="text-3xl font-semibold">TD</div>
             <nav className="grid grid-flow-col gap-8 text-lg font-medium">
               <a href="/">Home</a>
               <a>Work</a>
@@ -20,7 +20,13 @@ export default function Home() {
             </nav>
           </div>
         </header>
-      </div>
+        <main>
+          <section className="w-full flex justify-center items-center  pt-80"> 
+          
+          <h1 className="font-black text-6xl">Hello, Everyone</h1></section>
+
+        </main>
+      
     </>
   );
 }
