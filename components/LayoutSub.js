11690dd76917a363160978c905c5a6fa686cac76
Tushar Debnath <41472705+tushar7d@users.noreply.github.com>
2021-12-05 16:26:45 +0530

    changes

--- components/LayoutSub.js
+++ components/LayoutSub.js
@@ -1,28 +1,24 @@
-
-import ActiveLink from './ActiveLink'
-
+import ActiveLink from "./ActiveLink";
 
 const LayoutSub = (props) => {
   return (
     <div className="w-[350px] border-r ">
-      
       {props.posts.map((post, index) => (
-        <ActiveLink href={'/work/' + post.slug} key={index}>
-          <div className=" p-4 m-3 rounded-md hover:bg-gray-200 " style={{ maxWidth: '540px' }}>
-           
-                  <h5 className="">{post.frontMatter.title}</h5>
-                  <p className="">{post.frontMatter.description}</p>
-                  <p className="">
-                    <small className="">{post.frontMatter.date}</small>
-                  </p>
+        <ActiveLink href={"/work/" + post.slug} key={index}>
+          <div
+            className=" p-4 m-3 hover:rounded-md hover:bg-gray-200 border-b hover:border-b-0 "
+            style={{ maxWidth: "540px" }}
+          >
+            <h5 className="">{post.frontMatter.title}</h5>
+            <p className="">{post.frontMatter.description}</p>
+            <p className="">
+              <small className="">{post.frontMatter.date}</small>
+            </p>
           </div>
-              
         </ActiveLink>
       ))}
     </div>
-  )
-}
+  );
+};
 
-
-
-export default LayoutSub
+export default LayoutSub;
