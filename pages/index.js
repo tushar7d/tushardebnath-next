46b0af51b13476537270a3479d69313bd9a75153
Tushar Debnath <41472705+tushar7d@users.noreply.github.com>
2021-12-10 17:59:18 +0530

    changes

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 const Home = () => {
   return (
     <div className="home-container">
-      <div className="max-w-6xl mx-auto p-12 ">
+      
+      <div className="max-w-6xl mx-auto p-12">
+        
         <img src="./td.png" width="200px" className="mb-4" />
         <div className=" text-4xl font-black mb-4 antialiased ">Hello Everyone!</div>
         
-        <div className="font-serif text-2xl text-gray-800 leading-relaxed antialiased">
+        <div className=" text-2xl text-gray-800 leading-relaxed antialiased">
           I am Tushar a product designer and developer from New Delhi. Currently,
           I work at Zomato as senior product designer. When I am not designing
           or coding, you can find me reading fantasy books, working on my
