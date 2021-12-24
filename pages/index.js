40932d47ce31976501ba26db401062900ff7a82d
Tushar Debnath <txdebnath@gmail.com>
2021-12-24 21:06:56 +0530

    changes

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,4 @@
 import Head from "next/head";
-import React, { useRef, useState } from 'react'
-import { Canvas, useFrame } from "@react-three/fiber";
-
-function Box(props) {
-  // This reference will give us direct access to the mesh
-  const mesh = useRef()
-  // Set up state for the hovered and active state
-  const [hovered, setHover] = useState(false)
-  const [active, setActive] = useState(false)
-  // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame((state, delta) => (mesh.current.rotation.x += 0.01))
-  // Return view, these are regular three.js elements expressed in JSX
-  return (
-    <mesh
-      {...props}
-      ref={mesh}
-      scale={active ? 1.5 : 1}
-      onClick={(event) => setActive(!active)}
-      onPointerOver={(event) => setHover(true)}
-      onPointerOut={(event) => setHover(false)}
-    >
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-    </mesh>
-  )
-}
 
 const Home = () => {
   return (
@@ -32,16 +6,23 @@ const Home = () => {
       <Head>
         <title>Tushar Debnath</title>
       </Head>
-      
-      <div className="flex-auto mx-auto p-12  z-50 ">
-        <img src="./td.png" width={200} className="mx-auto mb-4" />
-        <div className=" font-serif text-6xl text-center font-bold mb-2 antialiased text-gray-800">
+
+      <div className=" mx-auto p-2 md:p-12  z-50 ">
+        <img src="./td.png" width={200} className="mx-auto mb-4 mt-6" />
+        <div className=" font-serif text-4xl md:text-5xl lg:text-6xl text-center font-bold mb-2 antialiased ">
           Tushar Debnath
         </div>
 
-        <div className=" text-xl font-medium mx-24  text-center text-gray-800  leading-normal antialiased">
+        <div className=" text-lg md:text-xl lg:text-2xl font-medium  text-center text-gray-800  leading-normal antialiased">
           Product Designer and Design Technologist.
         </div>
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-3 max-w-5xl mx-auto mt-12">
+        <div className="bg-gray-500 h-12">hello</div>
+        <div className="bg-gray-500 h-12">hello</div>
+        <div className="bg-gray-500 h-12">hello</div>
+
+        </div>
+        
       </div>
     </div>
   );
