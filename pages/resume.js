916f5f44772ae17fe2734bb100fa8ba8fa5d6aa4
Tushar Debnath <41472705+tushar7d@users.noreply.github.com>
2021-12-25 15:28:31 +0530

    changes

---
+++ pages/resume.js
@@ -0,0 +1,13 @@
+const Resume = () => {
+  return (
+   <div className="p-6">
+       <div className=" text-3xl mb-4  font-medium">Resume</div>
+       <div>Experience</div>
+       <div>Skills</div>
+       <div>Education</div>
+
+   </div>
+  );
+};
+
+export default Resume;
