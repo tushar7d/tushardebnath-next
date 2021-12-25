fd401c09c13928904516e9c05791312531451c9a
Tushar Debnath <41472705+tushar7d@users.noreply.github.com>
2021-12-25 18:40:43 +0530

    changes

--- pages/resume.js
+++ pages/resume.js
@@ -1,9 +1,10 @@
 const Resume = () => {
   return (
    <div className="p-6">
-       <div className=" text-3xl mb-4  font-medium">Resume</div>
+       <div className="text-3xl mb-4 font-medium ">Resume</div>
        <div>Experience</div>
        <div>Skills</div>
+       <div>Leadership Values</div>
        <div>Education</div>
 
    </div>
