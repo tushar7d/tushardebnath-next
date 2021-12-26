8133ac7717383303ac98129d302cee12b24a13e9
Tushar Debnath <txdebnath@gmail.com>
2021-12-26 16:22:17 +0530

    changes

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Image from "next/image";
 
 const Home = () => {
   return (
@@ -8,24 +9,33 @@ const Home = () => {
       </Head>
 
       <div className=" mx-auto p-2 md:p-12  z-50 ">
-        <img src="./td.png" width={200} className="mx-auto mb-4 mt-6" />
-        <div className=" font-serif text-4xl md:text-5xl lg:text-6xl text-center font-bold mb-2 antialiased ">
+        <div className=" flex justify-center mb-4 ">
+          <Image src="/td.png" width={200} height={200} loading="lazy" />
+        </div>
+
+        <div className=" font-serif text-4xl md:text-5xl  text-center font-bold mb-2 antialiased ">
           Tushar Debnath
         </div>
-
-        <div className=" text-lg md:text-xl lg:text-2xl font-medium  text-center text-gray-800  leading-normal antialiased">
-          Product Designer and Design Technologist.
+        <div className=" text-lg md:text-xl lg:text-2xl  text-center text-gray-500  leading-normal antialiased">
+          Product Designer and Design Technologist fron New Delhi.
         </div>
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-3 max-w-5xl mx-auto mt-12">
-        <div className="bg-gray-100 h-12 p-12">Section</div>
-        <div className="bg-gray-100 h-12 p-12">Section</div>
-        <div className="bg-gray-100 h-12 p-12">Section</div>
-
+        <div className=" grid grid-cols-1  md:grid-cols-1 lg:grid-cols-2  gap-3 max-w-5xl mx-auto mt-4 p-4">
+          <div className="p-4 bg-indigo-100 rounded-xl">
+            <h1 className="text-xl font-medium">About</h1>
+            <p>heloo this is awesome</p>
+          </div>
+          <div className="p-4 bg-indigo-100 rounded-xl">
+            <h1 className="text-xl font-medium">About</h1>
+            <p>heloo this is awesome</p>
+          </div>
+          <div className="p-4 bg-indigo-100 rounded-xl">
+            <h1 className="text-xl font-medium">About</h1>
+            <p>heloo this is awesome</p>
+          </div>
         </div>
-        
       </div>
     </div>
   );
 };
 
-export default Home;
+export default H ome;
