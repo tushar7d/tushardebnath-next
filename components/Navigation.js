ac2837a36f576c282cbf402cbe9b63bb7dd88088
Tushar Debnath <txdebnath@gmail.com>
2021-12-28 23:20:11 +0530

    changes

--- components/Navigation.js
+++ components/Navigation.js
@@ -31,7 +31,7 @@ const Navigation = () => {
             </div>
           </Link>
   
-          <LinkIn href="/work" name="Work" icon={<MdWork size={16} />} />
+          <LinkIn href="/projects" name="Projects" icon={<MdWork size={16} />} />
   
           <LinkIn href="/writing" name="Writing" icon={<FaPenNib size={16} />} />
   
