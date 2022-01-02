2a93dbe71169375ca5f8babcbe8f3ad1b7481eca
Tushar Debnath <41472705+tushar7d@users.noreply.github.com>
2022-01-03 00:30:55 +0530

    added back button

--- pages/projects/[slug].js
+++ pages/projects/[slug].js
@@ -3,6 +3,8 @@ import { MDXRemote } from "next-mdx-remote";
 import { getSanityContent } from "../../utils/sanity";
 import ProjectLayout from "../../components/ProjectLayout";
 import Callout from "../../components/Callout";
+import { FaChevronLeft } from "@react-icons/all-files/fa/FaChevronLeft";
+import Link from "next/link";
 
 const components = { Callout };
 
@@ -12,12 +14,24 @@ export default function TestPage({ source, po, img }) {
       <div className="hidden lg:block">
         <ProjectLayout src="/projects/" projects={po} />
       </div>
-      <article className="w-full max-w-6xl p-8 mx-auto overflow-scroll prose scrollbar-hide">
-        <img className="" src={img} />
+      <article className="w-full max-w-6xl ] mx-auto overflow-scroll  scrollbar-hide">
+        <Link href="/projects">
+        <div className="flex items-center p-2 border-b">
+          <button className="flex px-2 py-2 mr-2 text-sm text-gray-500 border rounded-md hover:bg-slate-200 hover:text-gray-900"><FaChevronLeft size={12} /> </button>
+          Back
+          </div>
+        
+        </Link>
+       
+        <div className="p-8">
+          <img className="mb-8" src={img} />
+          <div className="prose">
+            <MDXRemote {...source} components={components} />
+          </div>
 
-        <MDXRemote {...source} components={components} />
-        <a>Previous</a>
-        <a>Next</a>
+          <a>Previous</a>
+          <a>Next</a>
+        </div>
       </article>
     </div>
   );
