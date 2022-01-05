852544c4de8cb964169e222ce502341d4f10a25b
Tushar Debnath <txdebnath@gmail.com>
2022-01-05 18:14:07 +0530

    changes

--- components/Layout.js
+++ components/Layout.js
@@ -1,20 +1,12 @@
 import Navigation from "./Navigation";
 import HNavigation from "./HNavigation";
-import { useTheme } from "next-themes";
 
 const Layout = (props) => {
-  const { theme, setTheme } = useTheme();
+
 
   return (
     <div className="md:flex md:h-screen md:overflow-hidden ">
-      <button
-        aria-label="Toggle Dark Mode"
-        type="button"
-        className="absolute top-0 right-0 z-50 order-2 p-2 md:order-3"
-        onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-      >
-        Toggle Dark
-      </button>
+      
       <header className="hidden md:block lg:block">
         <Navigation />
       </header>
