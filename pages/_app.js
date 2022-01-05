a28f69a4fd219673042f60bf82ae6aaf2b460c12
Tushar Debnath <txdebnath@gmail.com>
2022-01-05 18:14:46 +0530

    changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,10 @@
 import "../styles/globals.css";
 import Head from "next/head";
-import { ThemeProvider } from 'next-themes'
 
 import Layout from "../components/Layout";
 
 function MyApp({ Component, pageProps }) {
- 
   return (
-    <ThemeProvider attribute="class">
     <div className=" md:overflow-hidden md:h-screen md:w-screen">
       <Head>
         <title>Tushar</title>
@@ -16,12 +13,7 @@ function MyApp({ Component, pageProps }) {
         <Component {...pageProps} />
       </Layout>
     </div>
-    </ThemeProvider>
   );
 }
-
-
-
-
 
 export default MyApp;
