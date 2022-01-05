c4c3cfd58ed755f50f87361767d48f01d2ab789c
Tushar Debnath <txdebnath@gmail.com>
2022-01-05 19:20:22 +0530

    changes

--- components/BlogLayout.js
+++ components/BlogLayout.js
@@ -5,24 +5,9 @@ const SubLayout = ({ pages,src }) => {
   const router = useRouter();
 
   return (
-    <div>
-      <div className="flex-initial block h-screen overflow-scroll divide-y scrollbar-hide lg:hidden">
-        {pages.map(({ title, slug, date, sub }) => (
-          <Link href={`${src}${slug}`} key={slug}>
-            <div
-              className={
-                router.asPath.startsWith(`${src}${slug}`)
-                  ? "link-sub-selected"
-                  : "link-sub"
-              }
-            >
-              <h5 className="text-base font-semibold">{title}</h5>
-              <p className="text-sm font-normal text-gray-700">{sub}</p>
-            </div>
-          </Link>
-        ))}
-      </div>
-      <div className="w-[350px] min-w-[350px] border-r  overflow-scroll scrollbar-hide  h-screen flex-initial hidden md:hidden lg:block  ">
+  
+      
+      <div className="md:w-[350px] md:min-w-[350px]  border-r overflow-scroll scrollbar-hide  h-screen flex-initial divide-y ">
         {pages.map(({ title, slug, date, sub }) => (
           <Link href={`${src}${slug}`} key={slug}>
             <div
@@ -33,14 +18,14 @@ const SubLayout = ({ pages,src }) => {
               }
             >
               
-
-              <h5 className="text-base font-semibold">{title}</h5>
-              <p className="text-sm font-normal text-gray-700">{sub}</p>
+              <small className="text-xs text-gray-500 uppercase ">type</small>
+                <p className="text-base font-semibold ">{title}</p>
+                <p className="text-sm font-normal text-gray-700 ">{sub}</p>
             </div>
           </Link>
         ))}
       </div>
-    </div>
+    
   );
 };
 
--- pages/writing/[slug].js
+++ pages/writing/[slug].js
@@ -1,6 +1,8 @@
 import { serialize } from "next-mdx-remote/serialize";
 import { MDXRemote } from "next-mdx-remote";
+import { FaChevronLeft } from "@react-icons/all-files/fa/FaChevronLeft";
 import { getSanityContent } from "../../utils/sanity";
+import Link from "next/link";
 import SubLayout from "../../components/BlogLayout";
 
 import Callout from "../../components/Callout";
@@ -8,13 +10,32 @@ import Callout from "../../components/Callout";
 const components = { Callout };
 
 export default function TestPage({ source, po }) {
+  
   return (
     <div className="flex flex-auto overflow-hidden md:h-screen">
       <div className="hidden lg:block">
         <SubLayout src="/writing/" pages={po} />
       </div>
-      <article className="mdx">
-        <MDXRemote {...source} components={components} />
+
+      <article className="w-full max-w-[850px] mx-auto overflow-scroll scrollbar-hide">
+        <Link href="/writing">
+          <div className="flex items-center p-2 border-b lg:hidden">
+            <button className="flex px-2 py-2 mr-2 text-sm text-gray-500 border rounded-md hover:bg-slate-200 hover:text-gray-900">
+              <FaChevronLeft size={12} />
+            </button>
+            Back
+          </div>
+        </Link>
+
+        <div className="p-8">
+          
+          <div className="prose max-w-none">
+            <MDXRemote {...source} components={components} />
+          </div>
+
+          <a>Previous</a>
+          <a>Next</a>
+        </div>
       </article>
     </div>
   );
