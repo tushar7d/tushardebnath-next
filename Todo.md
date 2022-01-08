b8d4372277f98c6eb3e56a7edb54e409e7bfeea0
Tushar Debnath <txdebnath@gmail.com>
2022-01-08 17:09:16 +0530

    changes

--- Todo.md
+++ Todo.md
@@ -8,3 +8,9 @@
 Hello, I am Tushar, I build digital products, custom design tools,
 workflows and design systems. For 8 years I've worked with teams
 applying human-centred design to challenges in Travel, Hyper Local, and Personal Communication.
+
+ A bit about me, I Love building digital products, design tools,
+          workflows and design systems. Currently, I work at Zomato as senior
+          product designer. When I am not designing or coding, you can find me
+          reading fantasy books, working on my podcast, or travelling with my
+          wife.
--- pages/index.js
+++ pages/index.js
@@ -20,14 +20,7 @@ const Home = () => {
         <div className="max-w-4xl mx-auto mb-3 text-xl antialiased font-medium leading-normal text-center text-gray-800 md:text-xl lg:text-2xl">
           Product Designer and Design Technologist fron New Delhi.
         </div>
-        <div className="mx-auto mb-6 text-lg text-justify text-gray-500 ">
-          
-          A bit about me, I Love building digital products, design tools,
-          workflows and design systems. Currently, I work at Zomato as senior
-          product designer. When I am not designing or coding, you can find me
-          reading fantasy books, working on my podcast, or travelling with my
-          wife.
-        </div>
+       
         <div className="flex justify-center w-full mx-auto space-x-3 ">
           <button className="p-2 px-4 text-white bg-black rounded-md">
            View Work
