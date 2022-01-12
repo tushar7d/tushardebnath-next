6a6865d3dd135840f0fff424cb611e9d1b132e77
Tushar Debnath <41472705+tushar7d@users.noreply.github.com>
2022-01-12 13:02:04 +0530

    changes

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,23 @@
 import Layout from "../components/Layout";
-import { getSanityContent } from "../utils/sanity";
-import { serialize } from "next-mdx-remote/serialize";
-import { MDXRemote } from "next-mdx-remote";
 
 const Home = ({ mdxSource }) => {
   return (
     <>
-      <div className="p-12 prose prose-base max-w-prose mx-auto">
-        <MDXRemote {...mdxSource} />
+      <div className="p-12 mx-auto prose prose-lg max-w-prose">
+        <h1>Hello</h1>
+        <p>
+          I'm Tushar, a product designer and developer, from New Delhi. I build
+          digital products,design tools, workflows and design systems. I am
+          currently working at Zomato as a senior product designer.
+        </p>
+        <p>
+          For the past 9 odd years I've worked with teams applying human-centred
+          design to challenges in Travel, Hyper Local, and Instant messaging.
+        </p>
+        <p>
+          When I am not designing or coding, you can find me reading fantasy
+          books, working on my cool side projects, or travelling with my wife.
+        </p>
       </div>
     </>
   );
@@ -15,23 +25,5 @@ const Home = ({ mdxSource }) => {
 Home.getLayout = function getLayout(page) {
   return <Layout>{page}</Layout>;
 };
-
-export async function getStaticProps() {
-  const data = await getSanityContent({
-    query: `
-    query AllSiteconfig {
-      allSiteconfig{
-       content
-      }
-      }
-    `,
-  });
-
-  const mdxSource = await serialize(data.allSiteconfig[1].content);
-
-  return {
-    props: { mdxSource },
-  };
-}
 
 export default Home;
