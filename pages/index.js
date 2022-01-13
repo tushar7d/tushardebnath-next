e8e73d0b7676c9c98fb38eb6b5e9efc5f7b4ac1b
Tushar Debnath <txdebnath@gmail.com>
2022-01-13 15:34:22 +0530

    changes

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Layout from "../components/Layout";
-import Img from "../components/Img";
+
 
 const Job = ({ name, role, tenure }) => {
   return (
@@ -13,64 +13,31 @@ const Job = ({ name, role, tenure }) => {
   );
 };
 
-
-
-const Home = ({ mdxSource }) => {
+const Home = () => {
   return (
     <>
-      <div className="p-8 mx-auto md:p-12 max-w-prose">
-        <Img s="/td.png" w={200} h={200} />
-
-        <div className="mb-8 prose prose-lg ">
-        <h2 className="mb-4 text-2xl font-semibold">About Me</h2>
-          <p className="text-justify">
-            Hello, I'm Tushar a product designer and developer, from New Delhi. 
-            I build digital products, workflows, design tools and design
-            systems. I am currently working at Zomato as a senior product
-            designer.
+      <div className="p-8 w-full mx-auto md:p-12 max-w-prose mt-12">
+        <img src="/td.png" className="w-[200px] mx-auto" />
+        
+      <h2 className="mb-2 text-4xl font-serif text-center font-semibold my-4">Tushar Debnath</h2>
+        
+          <p className="text-justify ">
+            I'm Tushar a product designer and developer, from New Delhi. I build
+            digital products, workflows, design tools and design systems.
           </p>
-          <p className="text-justify">
+          <p className="text-justify mb-2">
             For the past 9 odd years I've worked with different teams applying
             human-centred design to challenges in Travel, Hyper Local, and
             Instant messaging.
           </p>
-          <p className="text-justify">
+          <p className="text-justify mb-2">
             When I am not designing or coding, you can find me reading fantasy
             books, building Gundams, working on side projects, or travelling
             with my lovely wife.
           </p>
         </div>
-        <div>
-          <h2 className="mb-4 text-2xl font-semibold">Work Experience</h2>
-          <div className="divide-y ">
-            <Job
-              name="Zomato"
-              role="Senior Product Designer"
-              tenure="2019 - current"
-            />
-            <Job
-              name="Expedia"
-              role="Senior Product Designer"
-              tenure="2019 - current"
-            />
-            <Job
-              name="Hike Messenger"
-              role="Lead Product Designer"
-              tenure="2019 - current"
-            />
-            <Job
-              name="MakeMyTrip"
-              role="Product Designer"
-              tenure="2019 - current"
-            />
-            <Job
-              name="Frog Walks Out"
-              role="Designer"
-              tenure="2019 - current"
-            />
-          </div>
-        </div>
-      </div>
+        
+    
     </>
   );
 };
--- pages/resume.js
+++ pages/resume.js
@@ -1,15 +1,77 @@
+import Layout from "../components/Layout";
+
+
+const Job = ({ name, role, tenure }) => {
+  return (
+    <div className="flex items-center justify-between w-full py-4 ">
+      <div>
+        <div className="mb-1 text-xl font-semibold">{name}</div>
+        <div>{role}</div>
+      </div>
+      <div className="text-gray-500 tetx-sm">{tenure}</div>
+    </div>
+  );
+};
+
 const Resume = () => {
   return (
-   <div className="p-6 mx-auto">
-       <div className="text-3xl mb-4 font-semibold">Resume</div>
-       <div className="text-2xl font-medium mt-4">Download</div>
-       <div className="text-2xl font-medium mt-4">Experience</div>
-       <div className="text-2xl font-medium mt-4">Skills</div>
-       <div className="text-2xl font-medium mt-4">Values</div>
-       <div className="text-2xl font-medium mt-4">Education</div>
-
-   </div>
+    <>
+      <div className="p-8 mx-auto md:p-12 max-w-prose">
+        
+      <h2 className="mb-4 text-2xl font-semibold">About Me</h2>
+        <div className="mb-12 prose prose-lg ">
+          <p className="text-justify">
+            I'm Tushar a product designer and developer, from New Delhi. I build
+            digital products, workflows, design tools and design systems. I am
+            currently working at Zomato as a senior product designer.
+          </p>
+          <p className="text-justify">
+            For the past 9 odd years I've worked with different teams applying
+            human-centred design to challenges in Travel, Hyper Local, and
+            Instant messaging.
+          </p>
+          <p className="text-justify">
+            When I am not designing or coding, you can find me reading fantasy
+            books, building Gundams, working on side projects, or travelling
+            with my lovely wife.
+          </p>
+        </div>
+        <div>
+          <h2 className="mb-4 text-2xl font-semibold">Work Experience</h2>
+          <div className="divide-y ">
+            <Job
+              name="Zomato"
+              role="Senior Product Designer"
+              tenure="2019 - current"
+            />
+            <Job
+              name="Expedia"
+              role="Senior Product Designer"
+              tenure="2019 - current"
+            />
+            <Job
+              name="Hike Messenger"
+              role="Lead Product Designer"
+              tenure="2019 - current"
+            />
+            <Job
+              name="MakeMyTrip"
+              role="Product Designer"
+              tenure="2019 - current"
+            />
+            <Job
+              name="Frog Walks Out"
+              role="Designer"
+              tenure="2019 - current"
+            />
+          </div>
+        </div>
+      </div>
+    </>
   );
+};
+Resume.getLayout = function getLayout(page) {
+  return <Layout>{page}</Layout>;
 };
 
 export default Resume;
