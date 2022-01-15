336afbf72b8ca13b3c97bfabf16ce99a7de5b518
Tushar Debnath <41472705+tushar7d@users.noreply.github.com>
2022-01-15 13:57:53 +0530

    changes

--- pages/index.js
+++ pages/index.js
@@ -5,80 +5,67 @@ const Job = ({ name, role, tenure }) => {
   return (
     <div className="flex items-center justify-between w-full py-4 ">
       <div>
-        <div className="mb-1 text-xl font-semibold">{name}</div>
+        <div className="mb-1 text-lg font-semibold">{name}</div>
         <div>{role}</div>
       </div>
-      <div className="text-gray-500 tetx-sm text-right">{tenure}</div>
+      <div className="text-sm text-right text-gray-500">{tenure}</div>
     </div>
   );
 };
 
 const Home = () => {
   return (
-    <div className="grid h-screen xl:relative xl:overflow-hidden lg:grid-cols-2 xl:grid-cols-6  scrollbar-hide  ">
-      <div className="p-12 border-r xl:overflow-scroll xl:scrollbar-hide  xl:col-span-4 ">
-        <div className="max-w-prose mx-auto mt-24">
-          <img src="/td.png" className="w-[300px]  mx-auto" />
+    <div className="p-12 mx-auto mt-24 max-w-prose scrollbar-hide">
+      <img src="/td.png" className="w-[300px]  mx-auto" />
 
-          <div className="mt-12 mb-2 text-xl text-gray-400 ">INTRO </div>
+      <div className="mt-12 mb-2 text-xl text-gray-400 ">INTRO </div>
 
-          <div className="mb-6 text-4xl font-semibold ">
-            Hello 👋 I am Tushar a product designer and developer from New
-            Delhi.
-          </div>
-          <div className="text-xl tracking-wide text-justify text-gray-500">
-            <p className="mb-4">
-              I specialize in building digital products, optimizing workflows,
-              developing custom design tools and multi brand design systems.
-            </p>
-            <p className="mb-4">
-              For the past 9 odd years I've worked with different teams applying
-              human-centred design to challenges in Travel, Hyper Local, and
-              Instant messaging.
-            </p>
-            <p>
-              When I am not designing or coding, you can find me reading fantasy
-              books, working on side projects, or travelling with my lovely
-              wife.
-            </p>
-          </div>
-        </div>
+      <div className="mb-6 text-4xl font-semibold ">
+        Hello 👋 I am Tushar a product designer and developer from New Delhi.
       </div>
+      <div className="text-xl tracking-wide text-justify text-gray-500">
+        <p className="mb-4">
+          I specialize in building digital products, optimizing workflows,
+          developing custom design tools and multi brand design systems.
+        </p>
+        <p className="mb-4">
+          For the past 9 odd years I've worked with different teams applying
+          human-centred design to challenges in Travel, Hyper Local, and Instant
+          messaging.
+        </p>
+        <p>
+          When I am not designing or coding, you can find me reading fantasy
+          books, working on side projects, or travelling with my lovely wife.
+        </p>
 
-      <div className="xl:overflow-scroll  xl:scrollbar-hide p-12 xl:col-span-2   ">
-        <div className="   ">
-          <div className="mb-2 text-2xl text-gray-400 xl:mt-12 ">
-            EXPERIENCE{" "}
-          </div>
+        <div className="mt-12 mb-2 text-2xl text-gray-400 ">EXPERIENCE</div>
 
-          <div className="divide-y ">
-            {experience.map((data) => {
-              return (
-                <Job name={data.name} role={data.role} tenure={data.tenure} />
-              );
-            })}
-          </div>
+        <div className="divide-y ">
+          {experience.map((data) => {
+            return (
+              <Job name={data.name} role={data.role} tenure={data.tenure} />
+            );
+          })}
         </div>
-        <div className="">
-          <div className="mt-12 mb-2 text-2xl text-gray-400 ">EDUCATION</div>
 
-          <div className="divide-y ">
-            <Job
-              name="Bachelors in Technology (I.T)"
-              role="I.T.S Engineering College"
-              tenure="2010 - 2014"
-            />
-            <Job
-              name="Senior Secondary School"
-              role="I.V.P.School"
-              tenure="2008 - 20010"
-            />
-            <Job
-              name="Secondary School"
-              role="G.N.F.C.School"
-              tenure="2007 - 2008"
-            />
-          </div>
+        <div className="mt-12 mb-2 text-2xl text-gray-400 ">EDUCATION</div>
+
+        <div className="divide-y ">
+          <Job
+            name="Bachelors in Technology (I.T)"
+            role="I.T.S Engineering College"
+            tenure="2010 - 2014"
+          />
+          <Job
+            name="Senior Secondary School"
+            role="I.V.P.School"
+            tenure="2008 - 20010"
+          />
+          <Job
+            name="Secondary School"
+            role="G.N.F.C.School"
+            tenure="2007 - 2008"
+          />
         </div>
       </div>
     </div>
