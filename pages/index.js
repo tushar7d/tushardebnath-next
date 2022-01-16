f35b9666697dc2816f90a91f86a85397ca67b832
Tushar Debnath <41472705+tushar7d@users.noreply.github.com>
2022-01-16 19:59:45 +0530

    updated home page

--- pages/index.js
+++ pages/index.js
@@ -1,72 +1,18 @@
 import Layout from "../components/Layout";
-import experience from "../data";
-
-const Job = ({ name, role, tenure }) => {
-  return (
-    <div className="flex items-center justify-between w-full py-4 ">
-      <div>
-        <div className="mb-1 text-lg font-semibold">{name}</div>
-        <div>{role}</div>
-      </div>
-      <div className="text-sm text-right text-gray-500">{tenure}</div>
-    </div>
-  );
-};
+import Ticker from "react-ticker";
 
 const Home = () => {
   return (
-    <div className="max-w-3xl p-12 mx-auto mt-24 scrollbar-hide">
-      <img src="/td.png" className="w-[300px]  mx-auto" />
-
-      <div className="mt-12 mb-2 text-xl text-gray-400 ">INTRO </div>
-
-      <div className="mb-6 text-4xl font-semibold ">
-        Hello 👋 I am Tushar a product designer and developer from New Delhi.
+    <div className="p-8 mt-12 md:mt-0 md:p-12 lg:p-24">
+      <img src="/td.png" className="w-[250px] mb-12" />
+      <div className="mb-2 text-4xl font-medium">Hi, I'm Tushar.</div>
+      <div className="mb-6 text-6xl font-extrabold">
+        A product designer and developer from New Delhi.
       </div>
-      <div className="text-xl tracking-wide text-justify text-gray-500">
-        <p className="mb-4">
-          I specialize in building digital products, optimizing workflows,
-          developing custom design tools and multi brand design systems.
-        </p>
-        <p className="mb-4">
-          For the past 9 odd years I've worked with different teams applying
-          human-centred design to challenges in Travel, Hyper Local, and Instant
-          messaging.
-        </p>
-        <p>
-          When I am not designing or coding, you can find me reading fantasy
-          books, working on side projects, or travelling with my lovely wife.
-        </p>
-
-        <div className="mt-12 mb-2 text-2xl text-gray-400 ">EXPERIENCE</div>
-
-        <div className="divide-y ">
-          {experience.map((data) => {
-            return (
-              <Job name={data.name} role={data.role} tenure={data.tenure} />
-            );
-          })}
-        </div>
-
-        <div className="mt-12 mb-2 text-2xl text-gray-400 ">EDUCATION</div>
-
-        <div className="divide-y ">
-          <Job
-            name="Bachelors in Technology (I.T)"
-            role="I.T.S Engineering College"
-            tenure="2010 - 2014"
-          />
-          <Job
-            name="Senior Secondary School"
-            role="I.V.P.School"
-            tenure="2008 - 20010"
-          />
-          <Job
-            name="Secondary School"
-            role="G.N.F.C.School"
-            tenure="2007 - 2008"
-          />
-        </div>
+      <div className="text-xl">
+        I build digital products, custom design tools, workflows and design
+        systems. For the past 8 years I've worked with teams applying human-centred
+        design to the challenges in Travel, Hyper Local, and Personal Communication.
       </div>
     </div>
   );
@@ -75,4 +21,4 @@ Home.getLayout = function getLayout(page) {
   return <Layout>{page}</Layout>;
 };
 
-export default Home;
+export default Home
