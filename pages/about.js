e52488f5a21b0af9e467b4a4684d12c7f6ad9234
Tushar Debnath <41472705+tushar7d@users.noreply.github.com>
2022-06-10 01:52:53 +0530

    cleaned up home page

---
+++ pages/about.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export default function About() {
+
+    return (
+      <>
+        <Head>
+          <title>About Tushar</title>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        </Head>
+        <div className="grid lg:grid-cols-2 ">
+          <div>About me</div>
+          </div>
+      </>
+    );
+  }
