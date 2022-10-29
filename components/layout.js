cce739505c659c08b69b9bbdf6976a17c40c87b1
Tushar Debnath <41472705+tushar7d@users.noreply.github.com>
2022-10-29 23:49:51 +0530

    added placeholder

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ export default function Layout(props) {
   return (
     <div className="">
       <div className="w-full  h-[50vh] bg-gray-100">test</div>
-      <div className="max-w-3xl p-12 mx-auto prose bg-white shadow-xl -mt-44 rounded-xl ">
+      <div className="max-w-4xl p-12 mx-auto prose bg-white shadow-xl -mt-44 rounded-xl ">
         <Gallery>{props.children}</Gallery></div>
     </div>
   );
