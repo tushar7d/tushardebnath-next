a11b3b1b64388b593e4c9e2f5eda7c1198412bb0
Tushar Debnath <41472705+tushar7d@users.noreply.github.com>
2022-12-28 22:51:53 +0530

    chnages

--- pages/index.js
+++ pages/index.js
@@ -2,17 +2,22 @@ import Link from "next/link";
 
 const Tile = (props) => {
   return (
-    <Link href={props.link} >
-     
-      <div className={" flex flex-col justify-between   card p-0   bg-gray-100  rounded-b-md"}>
-      <div className=" rounded-t-md bg-[url('/zcov.png')] min-h-[240px] md:min-h-[180px]  bg-cover bg-no-repeat bg-center " />
-        <div className="p-4">
-          <div className="mb-1 text-xs text-gray-400">{props.type}</div>
-          <div className="text-sm font-medium ">{props.heading}</div>
-          
+    <Link href={props.link} target={props.ext ? "_blank" : "_self"}>
+      <div
+        className={
+          "flex sm:flex-col justify-between card md:h-[210px]"
+        }
+      >
+        <div className="w-1/3 sm:w-full">
+          {" "}
+          <img src={props.img} className="w-20 md:w-16" />
         </div>
-       
-        <div className="p-4 text-xs text-gray-400">{props.date}</div>
+
+        <div className="w-2/3 md:w-full">
+          <div className="mb-1 text-xs ">{props.type}</div>
+          <div className="mb-1 text-sm font-medium ">{props.heading}</div>
+          <div className="mb-1 text-xs ">{props.date}</div>
+        </div>
       </div>
     </Link>
   );
@@ -23,7 +28,7 @@ const Home = () => {
     <div className={"container"}>
       <div
         className={
-          "card p-8 md:col-span-3 md:flex md:justify-between md:items-center"
+          "card p-8 sm:col-span-2 md:col-span-3 md:flex md:justify-between md:items-center"
         }
       >
         <div className="mb-4 md:mb-0 md:order-last">
@@ -55,27 +60,62 @@ const Home = () => {
           <li className="p-4">Podcast</li>
         </ul>
       </div>
-      
+
       <Tile
         date="12 AUG 2022"
         type="CASE STUDY"
         heading="Zomato order tracker"
-        link="/"
+        link="/work/zomato-order-tracker"
+        img="./Zomato.png"
       />
       <Tile
         date="12 AUG 2022"
         type="CASE STUDY"
         heading="Expedia car rental experience"
-        link="/"
+        link="/work/expedia-rental-car"
+        img="./Expedia.png"
       />
       <Tile
         date="12 AUG 2022"
         type="FIGMA"
         heading="Color kit plugin"
-        link="/"
+        link="https://www.figma.com/community/plugin/797696673804519719/Color-Kit"
+        img="./colors.png"
+        ext
       />
 
-      
+      <Tile
+        date="12 AUG 2022"
+        type="FIGMA"
+        heading="Project Scaffold"
+        link="https://www.figma.com/community/plugin/747372158567878238/Project-Scaffold"
+        img="./scaffold.png"
+        ext
+      />
+      <Tile
+        date="12 AUG 2022"
+        type="MEDIUM"
+        heading="Automating our design system migration"
+        link="https://blog.prototypr.io/how-i-created-a-figma-plugin-to-automate-our-design-system-migration-600d1c07518e"
+        img="./dsauto.png"
+        ext
+      />
+      <Tile
+        date="12 AUG 2022"
+        type="MEDIUM"
+        heading="Figma shortcut using MIDI"
+        link="https://blog.prototypr.io/how-to-run-figma-shortcuts-using-midi-e0ab0997eafa"
+        img="./key.png"
+        ext
+      />
+      <Tile
+        date="12 AUG 2022"
+        type="MEDIUM"
+        heading="Consistent file stucture = better collaboration"
+        link="https://blog.prototypr.io/how-structuring-figma-files-in-a-consistent-way-has-improved-collaboration-and-efficiency-f3c904791b85"
+        img="./struct.png"
+        ext
+      />
     </div>
   );
 };
