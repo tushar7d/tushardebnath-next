e229a6b6f40e10988fe2076833a9b5e2eb07b073
Tushar <>
2023-01-04 19:39:47 +0530

    changes

--- pages/_app.js
+++ pages/_app.js
@@ -9,13 +9,19 @@ const opensans = Open_Sans({
 
 function MyApp({ Component, pageProps }) {
   return (
-    <main className={`${opensans.variable} font-sans`}  >
-      <div className="md:px-3 mt-0  md:mt-2 md:max-w-[1100px] mx-auto sticky top-0  z-50 bg-gray-100  ">
-      <div className="flex items-center content-center justify-between p-4 mx-auto mt-0 rounded-none sm:col-span-2 md:col-span-3 lg:col-span-4 card min-h-fit md:rounded-xl backdrop-blur-sm sm:bg-white">
-      <img src="./td.png" className="w-8" />
-      
-      
-      </div>
+    <main className={`${opensans.variable} font-sans pb-24`}>
+      <div className="fixed inset-x-0 bottom-0 mx-auto mb-8 w-fit md:relative md:mt-4 md:w-full md:max-w-[1100px] md:mb-4 px-3">
+        <div className="flex items-center content-center justify-between p-3 mx-auto mt-0 space-x-4 rounded-full shadow md:p-3 sm:col-span-2 md:col-span-3 lg:col-span-4 card min-h-fit md:rounded-xl md:shadow-none">
+          <img src="./td.png" className="w-8" />
+          <div>
+            <ul className="flex space-x-4 ">
+              
+              <li className="px-2 cursor-pointer hover:text-yellow-500">Work</li>
+              <li className="px-2 cursor-pointer hover:text-yellow-500">Blog</li>
+              <li className="px-2 cursor-pointer hover:text-yellow-500">About</li>
+            </ul>
+          </div>
+        </div>
       </div>
       <Component {...pageProps} />
     </main>
