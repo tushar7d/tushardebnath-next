972754f433f0d30bf84d7b7a76b43ec4281b5583
Tushar Debnath <41472705+tushar7d@users.noreply.github.com>
2023-01-08 23:18:49 +0530

    changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,7 @@
 import "../styles/globals.css";
-import Nav from "../components/nav";
-import { Open_Sans } from "@next/font/google";
-
-const opensans = Open_Sans({
-  subsets: ["latin"],
-  variable: "--font-open-sans",
-});
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <main className={`${opensans.variable} font-sans pb-24`}>
-     <Nav />
-      <Component {...pageProps} />
-    </main>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
