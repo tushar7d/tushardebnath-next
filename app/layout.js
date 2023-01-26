5363757b656e0b205627f5601a9f164237df98f9
Tushar <>
2023-01-26 14:17:09 +0530

    changes

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,14 @@ export default function RootLayout({ children }) {
   return (
     <html>
       <head />
-      <body>{children}</body>
+      <body className="top-container">
+        <nav className="nav-container">
+          <div>Logo</div>
+          <div>Links</div>
+          <div>Social</div>
+        </nav>
+        <main>{children}</main>
+       </body>
     </html>
   )
 }
