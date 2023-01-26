e84f39f00feb57a7b02e7313a08af323b4bfd878
Tushar <>
2023-01-26 15:09:15 +0530

    changes

--- app/page.js
+++ app/page.js
@@ -3,8 +3,8 @@ export default function Page() {
     <div className="home-content-container">
       <section className="home-content-section ">
         <div className="p-6 bg-green-100 rounded-2xl h-[600px] mb-4">Tushar Debnath</div>
-        <div className="p-6 bg-green-100 rounded-2xl h-[600px] mb-4">Tushar Debnath</div>
-        <div className="p-6 bg-green-100 rounded-2xl h-[600px] mb-4">Tushar Debnath</div>
+        <div className="p-6 bg-blue-100 rounded-2xl h-[600px] mb-4">Tushar Debnath</div>
+        <div className="p-6 bg-yellow-100 rounded-2xl h-[600px] mb-4">Tushar Debnath</div>
       </section>
     </div>
   );
