82b966e2a6ba7083ce630073167eb02050f0405f
Tushar <>
2023-01-26 16:39:44 +0530

    changes

---
+++ app/blog/layout.js
@@ -0,0 +1,8 @@
+
+export default function RootLayout({ children }) {
+  return (
+    <div className="bg-green-50">
+     {children}
+    </div>
+  )
+}
--- app/page.js
+++ app/page.js
@@ -2,9 +2,22 @@ export default function Page() {
   return (
     <div className="home-content-container">
       <section className="home-content-section ">
-        <div className="p-6 bg-green-100 rounded-2xl h-[600px] mb-4">Tushar Debnath</div>
-        <div className="p-6 bg-blue-100 rounded-2xl h-[600px] mb-4">Tushar Debnath</div>
-        <div className="p-6 bg-yellow-100 rounded-2xl h-[600px] mb-4">Tushar Debnath</div>
+        <div className="p-6 bg-green-100 rounded-2xl h-[250px] mb-4">
+          Hello I am tushar
+        </div>
+        <div className="p-6 bg-blue-100 rounded-2xl h-[250px] mb-4">
+          I am product designer and coder{" "}
+        </div>
+        <div className="p-6 bg-yellow-100 rounded-2xl h-[250px] mb-4">
+          I currently work for Revolut and I have previously worked at Expedia,
+          Zomato(Indian Unicorn), MakeMyTrip, Hike and more{" "}
+        </div>
+        <div className="p-6 bg-green-100 rounded-2xl h-[250px] mb-4">
+          I Live in New Delhi with my wife priya 
+        </div>
+        <div className="p-6 bg-blue-100 rounded-2xl h-[250px] mb-4">
+          When I am not working 
+        </div>
       </section>
     </div>
   );
