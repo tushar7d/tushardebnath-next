3077b4b1e22b6d77e375dcad8911a7bbd969d6f0
Tushar <>
2023-01-27 14:40:04 +0530

    changes

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,32 @@
 import "../styles/globals.css";
+import {
+  HomeIcon,
+  RectangleStackIcon,
+  BookOpenIcon,
+} from "@heroicons/react/24/outline";
+import { AiFillLinkedin, AiFillGithub } from "react-icons/ai";
+import { FiFigma } from "react-icons/fi";
+
 export default function RootLayout({ children }) {
   return (
     <html>
       <head />
       <body className="top-container">
         <nav className="nav-container">
-          <div>Logo</div>
-          <div>Links</div>
-          <div>Social</div>
+          <div className="text-3xl font-bold">TD</div>
+          <div className="w-6 h-6 space-y-6 text-blue-600">
+            <HomeIcon  />
+            <RectangleStackIcon  />
+            <BookOpenIcon  />
+          </div>
+          <div className="space-y-6 text-gray-800">
+            <AiFillLinkedin className="w-6 h-6 " />
+            <AiFillGithub className="w-6 h-6 " />
+            <FiFigma className="w-6 h-6 " />
+          </div>
         </nav>
         <main>{children}</main>
-       </body>
+      </body>
     </html>
-  )
+  );
 }
