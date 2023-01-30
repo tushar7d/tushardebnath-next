05848f1aea74d335208ed252fc60dc309c78e349
Tushar <>
2023-01-30 22:30:17 +0530

    changes

--- app/components/Navigation.js
+++ app/components/Navigation.js
@@ -19,8 +19,8 @@ let Bttn = (props) => {
       <div
         className={` hover:scale-110 transition-transform duration-200 ease-out w-[48px] h-[48px] p-2  rounded-xl flex   items-center  justify-center ${
           path == props.link
-            ? "text-white bg-blue-500"
-            : "text-blue-600 bg-blue-50"
+            ? "text-white bg-orange-400"
+            : "text-orange-500 bg-orange-50"
         } `}
       >
         {props.children}
--- app/page.js
+++ app/page.js
@@ -8,30 +8,27 @@ export default function Page() {
         initial={{ y: 300 }}
         transition={{ duration: 1, delay: 0.1 }}
       >
-        <div className="w-full px-3 mx-auto mt-24 font-serif text-center text-8xl">
+        <div className="w-full px-3 mx-auto mt-[25vh] mb-8 font-serif text-5xl font-medium text-center md:text-8xl">
           Hello Everyone!
         </div>
+        <p className="mx-auto mb-12 font-serif text-2xl leading-loose text-center md:w-2/4 md:text-5xl ">
+                I am <em className="text-orange-400 ">Tushar Debnath</em> a
+                product designer, developer, & writer from New Delhi. 
+              </p>
+            
 
         <section className="home-content-section ">
-          <div className="flex items-center justify-between p-6 mb-4 bg-blue-100 rounded-2xl ">
-            <div>
-              <h2 className="mb-2 font-serif text-4xl ">I am Tushar Debnath</h2>
-              <div className="w-3/4 text-xl font-light text-left ">
-                a product designer, developer, and writer from New Delhi. I am
-                currently working with <a>Revolut</a> as part of the Expansion
-                team.
-              </div>
-            </div>
-
-            <img src="./images/td.png" className="w-[180px]" />
-          </div>
+         
+            
+           
+          
           <div className="flex items-center justify-between p-6 mb-4 bg-green-100 rounded-2xl ">
             <div>
               <h2 className="mb-2 font-serif text-3xl ">I specialize in</h2>
               <div className="w-3/4 text-xl font-light text-left ">
                 working with the product and tech teams to translate ideas into
-                compelling products and systems used by millions of people around the globe.
-                
+                compelling products and systems used by millions of people
+                around the globe.
               </div>
             </div>
 
