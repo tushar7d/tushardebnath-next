c887092474695401bc4df085db3427c60e7f98f4
Tushar <>
2023-01-30 16:16:50 +0530

    changes

---
+++ app/blog/[slug]/page.js
@@ -0,0 +1,27 @@
+import { MDXRemote } from "next-mdx-remote/rsc";
+
+import { promises as fs } from "fs";
+
+import path from "path";
+
+export async function generateStaticParams() {
+  return [{ slug: "one" }, { slug: "two" }];
+}
+
+async function getPost(params) {
+  const mdxDirectory = path.join(process.cwd(), "app/blog/mdx/");
+  const res = await fs.readFile(mdxDirectory + params.slug + ".mdx", "utf8");
+
+  return res;
+}
+
+export default async function Page({ params }) {
+  const data = await getPost(params);
+  return (
+    <>
+      <div className=" max-w-[1000px] mx-auto w-full prose pt-12 px-3">
+        <MDXRemote source={data} />
+      </div>
+    </>
+  );
+}
