b31b9d148f171e9ff0928ababf1090617f110d55
Tushar <>
2023-01-30 16:51:28 +0530

    changes

--- app/components/Navigation.js
+++ app/components/Navigation.js
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import { usePathname } from 'next/navigation';
+import { usePathname } from "next/navigation";
 
 import {
   HomeIcon,
@@ -11,29 +11,40 @@ import {
 import { AiFillLinkedin, AiFillGithub } from "react-icons/ai";
 import { FiFigma } from "react-icons/fi";
 
+let Bttn = (props) => {
+  const path = usePathname();
+
+  return (
+    <Link href={props.link}>
+      <div
+        className={` hover:scale-110 transition-transform duration-200 ease-out w-[48px] h-[48px] p-2  rounded-xl flex   items-center  justify-center ${
+          path == props.link
+            ? "text-white bg-blue-500"
+            : "text-blue-600 bg-blue-50"
+        } `}
+      >
+        {props.children}
+      </div>
+    </Link>
+  );
+};
+
 const Navigation = () => {
-    const path = usePathname()
-   
   return (
     <nav className="nav-container">
       <div className="font-serif text-3xl font-medium ">T</div>
       <div className="flex flex-col space-y-3 ">
-      
-        <Link href="/">
-          <div className={` hover:scale-110 transition-transform duration-200 ease-out w-[48px] h-[48px] p-2  rounded-xl flex   items-center  justify-center ${path=="/"?"text-white bg-blue-500":"text-blue-600 bg-blue-50"} `}>
-            <HomeIcon className="w-6 h-6 " />
-          </div>
-        </Link>
-        <Link href="/blog">
-          <div className={ `  hover:scale-110 transition-transform duration-200 ease-out  w-[48px] h-[48px] p-2  rounded-xl flex   items-center  justify-center ${path=="/blog"?"text-white bg-blue-500":"text-blue-600 bg-blue-50"} `}>
-            <RectangleStackIcon className="w-6 h-6" />
-          </div>
-        </Link>
-        <Link href="/blog">
-          <div className={` hover:scale-110 transition-transform duration-200 ease-out w-[48px] h-[48px] p-2  rounded-xl flex   items-center  justify-center ${path=="/log"?"text-white bg-blue-500":"text-blue-600 bg-blue-50"} `}>
-            <BookOpenIcon className="w-6 h-6 " />
-          </div>
-        </Link>
+        <Bttn link="/">
+          <HomeIcon className="w-6 h-6 " />
+        </Bttn>
+
+        <Bttn link="/work">
+          <RectangleStackIcon className="w-6 h-6" />
+        </Bttn>
+
+        <Bttn link="/blog">
+          <BookOpenIcon className="w-6 h-6 " />
+        </Bttn>
       </div>
       <div className="space-y-6 text-gray-800">
         <AiFillLinkedin className="w-6 h-6 " />
@@ -44,5 +55,4 @@ const Navigation = () => {
   );
 };
 
-
-export default Navigation
+export default Navigation;
