86a279362ad5501277b7ebe279a95d86785b1865
Tushar Debnath <41472705+tushar7d@users.noreply.github.com>
2023-02-07 17:35:02 +0530

    changes

--- app/page.js
+++ app/page.js
@@ -8,11 +8,11 @@ export default function Page() {
         initial={{ y: 300,opacity:0 }}
         transition={{ duration: 1, delay: 0.1 }}
       >
-        <div className="w-full px-3 mx-auto mt-[25vh] mb-8 font-serif text-5xl font-medium text-center md:text-8xl">
+        <div className="w-full px-3 mx-auto mt-[25vh] mb-3 md:mb-8 font-serif text-5xl sm:text-6xl font-medium text-center md:text-8xl">
           Hello Everyone!
         </div>
 
-        <p className="mx-auto mb-12 font-serif text-2xl text-center md:w-2/4 md:text-5xl ">
+        <p className="w-full mx-auto mb-12 font-serif text-2xl text-center md:w-3/4 sm:text-3xl md:text-5xl">
           I am <em className="text-orange-400 ">Tushar Debnath</em> a product
           designer and developer from New Delhi.
         </p>
