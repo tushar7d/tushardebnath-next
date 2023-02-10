b1552f3dd13cc75fedf963f3aedbeb082d28c95a
Tushar Debnath <41472705+tushar7d@users.noreply.github.com>
2023-02-10 17:30:39 +0530

    changes

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -2,21 +2,21 @@ import { MDXRemote } from "next-mdx-remote/rsc";
 import { promises as fs } from "fs";
 import path from "path";
 
-
 async function listDir(path) {
   try {
     return await fs.readdir(path);
   } catch (err) {
-    console.error('Error occurred while reading directory!', err);
+    console.error("Error occurred while reading directory!", err);
   }
 }
 
 export async function generateStaticParams() {
-
   const mdxDirectory = path.join(process.cwd(), "app/blog/mdx/");
-
-console.log(await listDir(mdxDirectory))
-  return [{ slug: "automating-design-system-migration" }, { slug: "two" }];
+  let list = await listDir(mdxDirectory);
+  let out = list.map((o) => {
+    return { slug: o.replace(".mdx", "") };
+  });
+  return out;
 }
 
 async function getPost(params) {
