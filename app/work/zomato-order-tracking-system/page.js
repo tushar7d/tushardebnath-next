af3f241c1afdce605d36c90e6ea69828e1a0d178
Tushar <>
2023-02-13 23:13:53 +0530

    changes

--- app/work/zomato-order-tracking-system/page.js
+++ app/work/zomato-order-tracking-system/page.js
@@ -1,16 +1,20 @@
-
+"use client";
+import { motion } from "framer-motion";
 export default function Page() {
   return (
-    <div className="home-content-container">
-     
-        <section className="grid grid-cols-2 gap-6 home-content-section">
-          <div className="col-span-2 mb-12 font-serif text-6xl font-medium">
-            Zomato Order Tracker
-          </div>
-         
+    <div className="pt-0 home-content-container">
+      <motion.div
+        animate={{ opacity: 1 }}
+        initial={{ opacity: 0 }}
+        transition={{ duration: 1.5, delay: 0.2 }}
+      >
+        <div className="h-[80vh] bg-blue-50">Cover</div>
+        <div className="h-[20vh] p-12 flex justify-between">
+          <div>Hello</div>
+          <div>Hello</div>
           
-        </section>
-      
+        </div>
+      </motion.div>
     </div>
   );
 }
