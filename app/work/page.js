e3612cd5203278187212f8ae7d4a632ee0a12147
Tushar <>
2023-02-13 17:09:25 +0530

    chnages

--- app/work/page.js
+++ app/work/page.js
@@ -1,5 +1,7 @@
 "use client";
 import { motion } from "framer-motion";
+import Link from "next/link";
+
 export default function Page() {
   return (
     <div className="home-content-container">
@@ -12,15 +14,19 @@ export default function Page() {
           <div className="col-span-2 mb-12 font-serif text-6xl font-medium">
             Featured Work
           </div>
-          <div className="flex flex-col items-center justify-center col-span-2 p-6 bg-center bg-gradient-to-tr from-blue-300 via-cyan-500 to-purple-500 project-card ">
+          <Link
+            href="/work/zomato-order-tracking-system"
+            className="flex flex-col items-center justify-center col-span-2 p-6 bg-center bg-gradient-to-tr from-blue-300 via-cyan-500 to-purple-500 project-card "
+          >
             <div className="w-2/3 font-serif text-5xl font-medium text-center text-white ">
-             Zomato Order Tracking System 
+              Zomato Order Tracking System
             </div>
-            <div className="mt-3 text-4xl font-light text-white">A case study</div>
-          </div>
+            <div className="mt-3 text-4xl font-light text-white">
+              A case study
+            </div>
+          </Link>
           <div className="project-card ">Main content</div>
           <div className="project-card">Main content</div>
-          
         </section>
       </motion.div>
     </div>
---
+++ app/work/zomato-order-tracking-system/page.js
@@ -0,0 +1,16 @@
+
+export default function Page() {
+  return (
+    <div className="home-content-container">
+     
+        <section className="grid grid-cols-2 gap-6 home-content-section">
+          <div className="col-span-2 mb-12 font-serif text-6xl font-medium">
+            Zomato Order Tracker
+          </div>
+         
+          
+        </section>
+      
+    </div>
+  );
+}
