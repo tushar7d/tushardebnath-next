bae6b579686cc35ee7b50d8bdab7ac6b06bcbfc4
Tushar <>
2023-02-15 19:11:14 +0530

    chnages

--- app/blog/page.js
+++ app/blog/page.js
@@ -2,19 +2,7 @@ export default async function Page() {
   return (
     <>
       <div className=" max-w-[1000px] mx-auto w-full prose pt-12 px-3">
-        <div class=" min-h-screen flex items-center justify-center px-16">
-          <div class="relative w-full max-w-lg">
-            <div class="absolute top-0 -left-4 w-[150px] h-[150px] bg-purple-300 rounded-full mix-blend-multiply filter  opacity-70 animate-blob blur-xl"></div>
-            <div class="absolute top-0 -right-[150px]  w-[150px] h-[150px] bg-yellow-300 rounded-full mix-blend-multiply filter  opacity-70 animate-blob animation-delay-2000 blur-xl"></div>
-            <div class="absolute top-0 -right-10  w-[130px] h-[150px] bg-pink-300 rounded-full mix-blend-multiply filter  opacity-70 animate-blob animation-delay-2000 blur-xl"></div>
-            <div class="absolute top-0 -left-24 w-[150px] h-[150px] bg-red-300 rounded-full mix-blend-multiply filter  opacity-70 animate-blob blur-xl"></div>
-            <div class="absolute -bottom-8 left-[180px]  w-[150px] h-[150px] bg-yellow-300 rounded-full mix-blend-multiply filter  opacity-70 animate-blob animation-delay-2000 blur-xl"></div>
-            <div class="absolute -bottom-8 left-[210px]  w-[150px] h-[150px] bg-pink-300 rounded-full mix-blend-multiply filter  opacity-70 animate-blob animation-delay-4000 blur-xl"></div>
-            <div class="m-8 relative space-y-4">
-              <div className="font-serif text-5xl font-medium ">The Blog Experience</div>
-            </div>
-          </div>
-        </div>
+       
       </div>
     </>
   );
