4ba939e78e219deb006db362e687b525cc20d44c
Tushar Debnath <41472705+tushar7d@users.noreply.github.com>
2023-08-20 12:24:42 +0530

    changes

--- app/work/page.js
+++ app/work/page.js
@@ -11,10 +11,10 @@ export default function Page() {
         transition={{ duration: 1, delay: 0.1 }}
       >
         <section className=" max-w-[1200px] mx-auto mt-24 px-6">
-          <div className=" mb-12 font-serif text-6xl font-medium">
+          <div className="mb-12 font-serif text-6xl font-medium ">
             Featured
           </div>
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+          <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
             <div className="project-card bg-red-50 ">Revolut India launch</div>
             <div className="project-card bg-red-50 ">Revolut Snap</div>
             <div className="project-card bg-red-50 ">Revolut APAC</div>
@@ -23,7 +23,7 @@ export default function Page() {
           <div className="my-12 mt-16 font-serif text-6xl font-medium">
             Case studies
           </div>
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+          <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
            
           <div className="project-card bg-red-50 ">Zomato order tracker</div>
             <div className="project-card bg-red-50 ">Zomato Market</div>
@@ -32,7 +32,7 @@ export default function Page() {
           <div className="my-12 mt-16 font-serif text-6xl font-medium">
             Side projects
           </div>
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+          <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
             <div className="project-card bg-red-50 ">Expedia design ops</div>
             <div className="project-card bg-red-50 ">Figma comunity plugins</div>
             <div className="project-card bg-red-50 ">Figma course</div>
