06a411490805f9130c92b59edff54e454f6d764f
Tushar <41472705+tushar7d@users.noreply.github.com>
2023-10-13 23:19:40 +0530

    changes

--- app/blog/layout.js
+++ app/blog/layout.js
@@ -1,7 +1,7 @@
 "use client";
 import { usePathname } from "next/navigation";
 import CardList from "./CardLayout";
-
+import Link from "next/link";
 
 import data from './data'
 
@@ -12,16 +12,18 @@ export default async function RootLayout({ children }) {
 
 console.log(path)
   return (
-    <div className="flex w-screen sm:w-[calc(100vw-80px)] h-screen sticky top-0 overflow-hidden ">
+    <div className="flex w-screen md:w-[calc(100vw-80px)] h-screen sticky top-0 overflow-hidden ">
       <div className={`${path.endsWith("/blog")?"block":"hidden md:block"}`}>
       <CardList  list={data}/>
       </div>
       
-      <div className="mx-auto">
-      <div className={`${path.endsWith("/blog")?"block":"hidden"}`}>
-      <div>Back</div>
-      </div>
-      {children}
+      <div className="mx-auto  overflow-scroll scrollbar-hide  ">
+     
+      <div className={`  cursor-pointer fixed bg-gray-50 p-4  w-full ${path.endsWith("/blog")?"hidden":"block md:hidden "}`}><Link href={"/blog"} className="p-2 rounded-xl hover:bg-gray-300 bg-gray-300 sm:bg-gray-50" >Back </Link></div>
+  <div className="mt-3">
+  {children}
+  </div>
+      
       </div>
     </div>
   );
